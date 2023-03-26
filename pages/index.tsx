90391440c1095e4ef9a17f3a6b30459ade5454e7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-26 21:21:17 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from './components/head'
 const Home = () => {
  return 
+<div>
 <Head />
 <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
@@ -176,6 +177,6 @@ const Home = () => {
     </div>
   </div>
 </section>
-
+<div>
 };
 export default Home;
