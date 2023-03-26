d2f4eb38d0ea81caa53b1774895dcd6945f510c0
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-26 15:20:58 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,7 +6,7 @@ export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       <Head />
-      <Component {...pageProps} />
     </div>
+          <Component {...pageProps} />
   );
 }
