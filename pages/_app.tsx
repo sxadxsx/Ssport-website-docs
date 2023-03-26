ffd428ba9d6e0875740719e93bb0f05cdd840b6b
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-26 17:44:58 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,13 @@
 import '@/styles/globals.css'
-import React, { Fragment } from 'react'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 
 export default function App({ Component, pageProps }: AppProps) {
-  <Fragment>
-    <Head />
- <Component {...pageProps} />
-  </Fragment>
-}
+  return (
+    <div>
+      <Head />
+<Component {...pageProps} />
+    </div>
 
+  );
+}
