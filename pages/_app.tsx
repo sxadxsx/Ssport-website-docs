a5cf53b929acaa980cbccd2bd0ffbb9c349bfd52
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-26 21:16:00 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,9 +4,9 @@ import Head from './components/head'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <div>
+    
 <Component {...pageProps} />
-    </div>
+    
 
   );
 }
