7eff56a5b67a6f7552ccd5fd1b43578d3d4c2edc
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-26 15:09:15 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,9 @@
+import head from './components/head'
 
 const Home = () => {
- return <section className="bg-white dark:bg-gray-900">
+ return 
+<head />
+<section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <a
       href="#"
