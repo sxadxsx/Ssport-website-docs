e948a09c75cca45ef3c6d799dc072b7fcf7f89e3
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-27 21:51:08 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -1,4 +1,4 @@
-export default function contact() {
+export function contact() {
   return (
       <section className="bg-white dark:bg-gray-900">
         <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
@@ -35,4 +35,4 @@ export default function contact() {
 
     );
   }
-export default contact;
+export contact;
