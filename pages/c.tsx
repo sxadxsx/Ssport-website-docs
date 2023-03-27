16b935d96e5f3636ca767a156129b0a7867e7ee5
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-27 22:18:22 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -24,8 +24,8 @@ function c() {
           id="email"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="name@flowbite.com"
-          required="email"
-        />
+          reqpuired
+          />
       </div>
       <div>
         <label
@@ -39,8 +39,8 @@ function c() {
           id="subject"
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="Let us know how we can help you"
-          required=""
-        />
+          reqpuired
+          />
       </div>
       <div className="sm:col-span-2">
         <label
