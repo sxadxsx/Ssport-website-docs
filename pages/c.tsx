f548732a9d0b8ce386c820fd31db7d6f900d41c1
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-27 22:13:55 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -24,7 +24,7 @@ function c() {
           id="email"
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="name@flowbite.com"
-          required=""
+          required="email"
         />
       </div>
       <div>
