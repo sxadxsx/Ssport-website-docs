fb34c6c711ca431f61703d70528ef24979f03e15
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-28 18:11:01 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -1,48 +1,44 @@
-import { Navbar } from "flowbite-react";
-import React from 'react'
-import Link from 'next/link'
+import Navbar from "flowbite-react";
+import React from 'react';
+import Link from 'next/link';
 
 function head() {
   return (
-   <Navbar
-  fluid={true}
-  rounded={true}
->
-        <Navbar.Brand>
-  <Link href="/" legacyBehavior>
+    <Navbar fluid={true} rounded={true}>
+      <Navbar.Brand>
+        <Link href="/" legacyBehavior>
+          <a>
+            <img
+              src="https://flowbite.com/docs/images/logo.svg"
+              className="mr-3 h-6 sm:h-9"
+              alt="Flowbite Logo"
+            />
+            <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+              Flowbite
+            </span>
+          </a>
         </Link>
-      <img
-      src="https://flowbite.com/docs/images/logo.svg"
-      className="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
-    />
-    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Flowbite
-    </span>
-   </Navbar.Brand>
-  <Navbar.Toggle />
-  <Navbar.Collapse>
-    <Navbar.Link
-      href="/navbars"
-      active={true}
-    >
-      Home
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      About
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      Services
-    </Navbar.Link>
-    <Navbar.Link href="/navbars">
-      Pricing
-    </Navbar.Link>
-    <Link href="/c" legacyBehavior>
-      <a>About Us</a>
-    </Link>
-  </Navbar.Collapse>
-</Navbar>
+      </Navbar.Brand>
+      <Navbar.Toggle />
+      <Navbar.Collapse>
+        <Navbar.Link href="/navbars" active={true}>
+          Home
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">
+          About
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">
+          Services
+        </Navbar.Link>
+        <Navbar.Link href="/navbars">
+          Pricing
+        </Navbar.Link>
+        <Link href="/c" legacyBehavior>
+          <a>About Us</a>
+        </Link>
+      </Navbar.Collapse>
+    </Navbar>
   )
 }
 
-export default head
+export default head;
