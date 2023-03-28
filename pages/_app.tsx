744f393baa86e834968a56df34f40950ce742643
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-28 21:54:44 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,14 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
+import footer from './components/footer'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
     <Head />
 <Component {...pageProps} />
+      <footer />
     </div>
 
   );
