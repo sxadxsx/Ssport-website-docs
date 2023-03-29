82be7ef71d3fe296567cded96a560a93b361b777
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-29 22:31:37 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,12 +27,8 @@ const Home = () => {
       </svg>
     </a>
     <h1 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      We invest in the worlds potential
+      我們的使命是將雙龍體育組成最棒體育組織
     </h1>
-    <p className="mb-8 text-lg font-normal text-gray-500 lg:text-xl sm:px-16 xl:px-48 dark:text-gray-400">
-      Here at Flowbite we focus on markets where technology, innovation, and
-      capital can unlock long-term value and drive economic growth.
-    </p>
     <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
       <a
         href="#"
