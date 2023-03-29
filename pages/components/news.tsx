0ad9ebc75f0fe547cffb2597ebced3006ec1689b
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-29 21:37:36 +0800

    Update news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -1,6 +1,7 @@
 import Link from 'next/link';
 
 function news() {
+    return (
 <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md sm:text-center">
