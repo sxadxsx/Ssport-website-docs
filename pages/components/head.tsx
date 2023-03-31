c4e680ffbb95735b30c28bea762d981e935bcc9d
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-31 23:47:49 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -9,7 +9,7 @@ function head() {
     <br />
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link href="/" legacyBehavior>
+    <Link href="/" className="flex items-center">
       <img
         src="https://flowbite.com/docs/images/logo.svg"
         className="h-8 mr-3"
@@ -80,14 +80,19 @@ function head() {
           </a>
         </li>
         <li>
-          <Link href="/c" legacyBehavior>
-      <a>聯繫</a>
-    </Link>
+          <a
+            href="#"
+            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+          >
+            Contact
+          </a>
         </li>
       </ul>
     </div>
   </div>
 </nav>
+
+
 </header>
       </div>
 
