e7101506f54a43daa120834d6d44659fbea2b5db
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-31 23:44:16 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -4,7 +4,8 @@ import Link from 'next/link';
 
 function head() {
   return (
-  <br />
+  <header>
+    <br />
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
     <Link href="/" legacyBehavior>
@@ -86,7 +87,7 @@ function head() {
     </div>
   </div>
 </nav>
-
+</header>
 
   )
 }
