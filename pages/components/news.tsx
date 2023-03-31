5809cdb15af40a3f35ca138cdca9c93ac049b9b4
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-31 21:42:58 +0800

    Update news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -6,12 +6,8 @@ function news() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md sm:text-center">
       <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
-        Sign up for our newsletter
+        訂閱最新消息
       </h2>
-      <p className="mx-auto mb-8 max-w-2xl font-light text-gray-500 md:mb-12 sm:text-xl dark:text-gray-400">
-        Stay up to date with the roadmap progress, announcements and exclusive
-        discounts feel free to sign up with your email.
-      </p>
       <form action="#">
         <div className="items-center mx-auto mb-3 space-y-4 max-w-screen-sm sm:flex sm:space-y-0">
           <div className="relative w-full">
@@ -34,7 +30,7 @@ function news() {
             </div>
             <input
               className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-              placeholder="Enter your email"
+              placeholder="輸入您的電子郵件"
               type="email"
               id="email"
             />
@@ -44,19 +40,19 @@ function news() {
               type="submit"
               className="py-3 px-5 w-full text-sm font-medium text-center text-white rounded-lg border cursor-pointer bg-primary-700 border-primary-600 sm:rounded-none sm:rounded-r-lg hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
             >
-              Subscribe
+              訂閱
             </button>
           </div>
         </div>
         <div className="mx-auto max-w-screen-sm text-sm text-left text-gray-500 newsletter-form-footer dark:text-gray-300">
-          We care about the protection of your data.{" "}
+         我們關心保護您的數據。{" "}
           <a
             href="#"
             className="font-medium text-primary-600 dark:text-primary-500 hover:underline"
           >
-            Read our Privacy Policy
+           閱讀我們的隱私政策
           </a>
-          .
+          。
         </div>
       </form>
     </div>
