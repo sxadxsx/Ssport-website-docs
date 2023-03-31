50fb92fa8c652c70acadf4f9b1fabaf3144a73dc
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-01 00:31:46 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,6 @@
 
 const Home = () => {
- return 
- <section className="bg-white dark:bg-gray-900">
+  return <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <a
       href="#"
@@ -177,6 +176,7 @@ const Home = () => {
     </div>
   </div>
 </section>
+
 
 };
 export default Home;
