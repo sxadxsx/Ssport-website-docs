5eff136c742a7e5bc4d764e3d7a10ef4ca9dbaaf
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-03-31 23:41:13 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -8,33 +8,23 @@ function head() {
   <br />
 <nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
   <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <a
-      href="https://nextjs-newhome-oh33.vercel.app/"
-      className="flex items-center"
-    >
+    <Link href="/" legacyBehavior>
       <img
-        src="https://i.imgur.com/GhQm5zm.png"
+        src="https://flowbite.com/docs/images/logo.svg"
         className="h-8 mr-3"
         alt="Flowbite Logo"
       />
-    </a>
-    <div
-      className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-      id="navbar-sticky"
-<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link href="/">
-      <img
-        src="https://i.imgur.com/GhQm5zm.png"
-        className="h-8 mr-3"
-        alt="ssport Logo"
-      />
       <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-       
+        Flowbite
       </span>
     </Link>
     <div className="flex md:order-2">
-      
+      <button
+        type="button"
+        className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+      >
+        Get started
+      </button>
       <button
         data-collapse-toggle="navbar-sticky"
         type="button"
@@ -89,8 +79,9 @@ function head() {
           </a>
         </li>
         <li>
-                  <Link href="/c">聯繫</Link>
-          </a>
+          <Link href="/c" legacyBehavior>
+      <a>聯繫</a>
+    </Link>
         </li>
       </ul>
     </div>
