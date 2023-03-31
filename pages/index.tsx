6805fcd49981a8e25ad17a9c9c797156055b7ff4
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-01 00:27:37 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,6 @@
-import Head from 'next/head'
+
 const Home = () => {
  return 
-<div>
-<Head>
- <title>雙龍體育</title>
-</Head>
- </div>
  <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <a
