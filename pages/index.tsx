a7884585928d75e9c22ab5efad154b60a9513398
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-01 12:44:19 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -9,7 +9,6 @@ const Home = () => {
           content="​雙龍職籃 ... 籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止 ..."
         />
       </Head>
-<p>
 <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <a
@@ -186,7 +185,6 @@ const Home = () => {
     </div>
   </div>
 </section>
-   <p> 
   </>
 
 };
