8fdec36987da8e134c647fbb5b0ee2decbebd5a1
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 13:24:44 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,6 @@
 import Hero from './components/hero';
 import Table from './components/table';
-import react from './components/table';
 import React from 'react';
-
 const Home = () => {
   return (
     <div>
