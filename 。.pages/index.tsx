b536be45231e9360c68e80dcf00f7af9f1a595aa
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 13:22:43 +0800

    Update and rename pages/index.tsx to 。.pages/index.tsx

--- 。.pages/index.tsx
+++ 。.pages/index.tsx
@@ -1,5 +1,7 @@
 import Hero from './components/hero';
 import Table from './components/table';
+import react from './components/table';
+import React from 'react';
 
 const Home = () => {
   return (
