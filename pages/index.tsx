2efda8cc18d69f1ef82e312e46d31af68b2f8553
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 14:25:04 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,15 +6,18 @@ import { NextSeo } from 'next-seo';
 
 const Home = () => {
   return (
-    <NextSeo
-      title="雙龍體育"
-      description="籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運...."
-    />
-    <div>
-      <Hero />
-      <Table />
-      <Tabl />
-    </div>
+    <>
+      <NextSeo
+        title="雙龍體育"
+        description="籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運...."
+      />
+      <div>
+        <Hero />
+        <Table />
+        <Tabl />
+      </div>
+    </>
   );
 }
+
 export default Home;
