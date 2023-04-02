1e2cdc47eb8dd64569ece1b6474258a70d848983
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 13:30:13 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -15,7 +15,7 @@ function table() {
     />
     <div className="mt-4 md:mt-0">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        Let's create more tools and ideas that brings us together.
+        Lets create more tools and ideas that brings us together.
       </h2>
       <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
         Flowbite helps you connect with friends and communities of people who
