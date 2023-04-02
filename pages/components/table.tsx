367548e5665b44c8601c5357fdd6b5770de326df
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 13:29:42 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -1,10 +1,18 @@
 import Link from 'next/link';
+import Image from 'next/image'
 
 function table() {
   return (
 <section className="bg-white dark:bg-gray-900">
   <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-
+    <Image
+      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+      alt="Picture of the author"
+    />
+    <Image
+      src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+      alt="Picture of the author"
+    />
     <div className="mt-4 md:mt-0">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
         Let's create more tools and ideas that brings us together.
