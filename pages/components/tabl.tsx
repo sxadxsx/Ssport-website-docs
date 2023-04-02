323950c2ecd72178c4651485870b21719f4e768a
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-02 13:49:13 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -7,18 +7,16 @@ function Tabl() {
       <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <div className="mt-4 md:mt-0">
           <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-            Lets create more tools and ideas that brings us together.
+            雙龍職籃
           </h2>
           <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
-            Flowbite helps you connect with friends and communities of people who
-            share your interests. Connecting with your friends and family as well as
-            discovering new ones is easy with features like Groups.
+           籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止對方獲得控球權或得分。籃球是世界上最多人觀看
           </p>
           <a
             href="#"
             className="inline-flex items-center text-white bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:focus:ring-primary-900"
           >
-            Get started
+           更多資訊
             <svg
               className="ml-2 -mr-1 w-5 h-5"
               fill="currentColor"
@@ -35,7 +33,7 @@ function Tabl() {
         </div>
         <Image
           className="w-full"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/cta/cta-dashboard-mockup.svg"
+          src="/sba.png"
           alt="dashboard image"
         />
       </div>
