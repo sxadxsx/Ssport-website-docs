1f19eff3ee76a4cced7d3598f1d295d093c5d2c0
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-03 09:47:28 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -21,7 +21,6 @@ function c() {
         <input
           type="email"
           id="email"
-          required
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="peter@ssangyongsports.org"
           />
@@ -42,7 +41,6 @@ function c() {
         <input
           type="text"
           id="subject"
-          required
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="讓我們得知如何幫助你"
           />
@@ -51,14 +49,12 @@ function c() {
         <label
           htmlFor="message"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
-          required
         >
           內容
         </label>
         <textarea
           id="message"
           rows={6}
-          required
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="Leave a comment..."
           defaultValue={""}
