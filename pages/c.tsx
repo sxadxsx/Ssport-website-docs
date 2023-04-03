2f0492b4ca61974ddaebfe58208ccd5981088603
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-03 10:00:48 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -43,6 +43,7 @@ function c() {
         <input
           type="text"
           id="subject"
+          required={true}
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
           placeholder="讓我們得知如何幫助你"
           />
@@ -57,6 +58,7 @@ function c() {
         <textarea
           id="message"
           rows={6}
+          required={true}
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="Leave a comment..."
           defaultValue={""}
