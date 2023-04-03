22f565d26e59ef1c9221015b35e6df628a6308ae
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-03 09:40:42 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -5,11 +5,10 @@ function c() {
    <section className="bg-white dark:bg-gray-900">
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
-      Contact Us
+      聯繫我們
     </h2>
     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
-      Got a technical issue? Want to send feedback about a beta feature? Need
-      details about our Business plan? Let us know.
+      有任何問題，立刻聯繫我們😊
     </p>
     <form action="https://submit-form.com/yFAbXMpc" className="space-y-8">
       <div>
@@ -17,13 +16,14 @@ function c() {
           htmlFor="email"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
         >
-          Your email
+          你的電子郵件
         </label>
         <input
           type="email"
           id="email"
+          required
           className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          placeholder="name@flowbite.com"
+          placeholder="peter@ssangyongsports.org"
           />
       </div>
       <input
@@ -37,25 +37,28 @@ function c() {
           htmlFor="subject"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
         >
-          Subject
+          主旨
         </label>
         <input
           type="text"
           id="subject"
+          required
           className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          placeholder="Let us know how we can help you"
+          placeholder="讓我們得知如何幫助你"
           />
       </div>
       <div className="sm:col-span-2">
         <label
           htmlFor="message"
           className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-400"
+          required
         >
-          Your message
+          內容
         </label>
         <textarea
           id="message"
           rows={6}
+          required
           className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg shadow-sm border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
           placeholder="Leave a comment..."
           defaultValue={""}
