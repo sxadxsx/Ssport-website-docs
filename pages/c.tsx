4961affe5eb13980bdade252b0f7298261dae940
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-03 09:58:51 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -12,18 +12,20 @@ function c() {
     </p>
     <form action="https://submit-form.com/yFAbXMpc" className="space-y-8">
       <div>
-        <label
-          htmlFor="email"
-          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
-        >
-          你的電子郵件
-        </label>
-        <input
-          type="email"
-          id="email"
-          className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-          placeholder="peter@ssangyongsports.org"
-          />
+       <label
+  htmlFor="email"
+  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+>
+  你的電子郵件
+</label>
+<input
+  type="email"
+  id="email"
+  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+  placeholder="peter@ssangyongsports.org"
+  required={true}
+/>
+
       </div>
       <input
     type="hidden"
