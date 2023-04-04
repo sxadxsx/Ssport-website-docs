cb61de05382628450f16d7d1e48b1bdb13e0ae98
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-04 12:49:06 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -1,4 +1,4 @@
-import { Button } from "flowbite-react";
+import { Navbar } from "flowbite-react";
 import React from 'react';
 import Link from 'next/link';
 import { Collapse } from "flowbite";
@@ -7,92 +7,54 @@ import type { CollapseOptions, CollapseInterface } from "flowbite";
 function head() {
   return (
   <div>
-    <header>
-<br />
-    <br />
-<nav className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600">
-  <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-    <Link href="/" className="flex items-center">
-      <img
-        src="https://i.imgur.com/GhQm5zm.png"
-        className="h-8 mr-3"
-        alt="雙龍體育 Logo"
-      />
-      <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-        
-      </span>
-    </Link>
-    <div className="flex md:order-2">
-      <button
-        data-collapse-toggle="navbar-sticky"
-        type="button"
-        className="inline-flex items-center p-2 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
-        aria-controls="navbar-sticky"
-        aria-expanded="false"
-      >
-        <span className="sr-only">Open main menu</span>
-        <svg
-          className="w-6 h-6"
-          aria-hidden="true"
-          fill="currentColor"
-          viewBox="0 0 20 20"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            fillRule="evenodd"
-            d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
-            clipRule="evenodd"
-          />
-        </svg>
-      </button>
-    </div>
-    <div
-      className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
-      id="navbar-sticky"
+<Navbar
+  fluid={true}
+  rounded={true}
+>
+  <Navbar.Brand
+    as={{
+      $$typeof: Symbol(react.forward_ref),
+      render: LinkWithRef
+    }}
+    <Link href="/">Home</Link>
+  >
+    <img
+      src="https://flowbite.com/docs/images/logo.svg"
+      className="mr-3 h-6 sm:h-9"
+      alt="Flowbite Logo"
+    />
+    <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
+      
+    </span>
+  </Navbar.Brand>
+  <Navbar.Toggle />
+  <Navbar.Collapse>
+    <Navbar.Link
+      href="/navbars"
+      active={true}
     >
-      <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 md:dark:text-blue-500"
-            aria-current="page"
-          >
-            Home
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            About
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            Services
-          </a>
-        </li>
-        <li>
-          <Link
-            href="/c"
-            className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:p-0 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
-          >
-            聯絡
-          </Link>
-        </li>
-      </ul>
-    </div>
-  </div>
-</nav>
-
-
-</header>
-      </div>
-
+      Home
+    </Navbar.Link>
+    <Navbar.Link
+      as={{
+        $$typeof: Symbol(react.forward_ref),
+        render: LinkWithRef
+      }}
+      to="/navbars"
+    >
+      About
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Services
+    </Navbar.Link>
+    <Navbar.Link href="/navbars">
+      Pricing
+    </Navbar.Link>
+   <Link href="/c" legacyBehavior>
+      聯繫
+    </Link>
+  </Navbar.Collapse>
+</Navbar>
   )
 }
 
