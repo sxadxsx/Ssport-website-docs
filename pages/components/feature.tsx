b3a671db77a82172784418ee5961f8ad47640000
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-05 03:10:07 +0800

    Update feature.tsx

--- pages/components/feature.tsx
+++ pages/components/feature.tsx
@@ -1,6 +1,6 @@
 
-
 function feature() {
+return (
 <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
     <div className="max-w-screen-md mb-8 lg:mb-16">
