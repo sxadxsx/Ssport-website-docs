70929cdc9fd55626754f162acdd551fc32e28d07
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-04 23:49:19 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -9,14 +9,14 @@ function head() {
   rounded={true}
     className="sticky-top"
 >
-  <Navbar.Brand href="https://flowbite.com/">
+  <Navbar.Brand>
+    <Link href="/"></Link>
     <img
-      src="https://flowbite.com/docs/images/logo.svg"
+      src="https://imgur.io/GhQm5zm.png"
       className="mr-3 h-6 sm:h-9"
-      alt="Flowbite Logo"
+      alt="ssport Logo"
     />
     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-      Flowbite
     </span>
   </Navbar.Brand>
   <div className="flex md:order-2">
