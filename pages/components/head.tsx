69fc20d7763f2019f2dd8834c14d87424d8ca2d0
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-04 23:58:17 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -7,7 +7,7 @@ function head() {
   <Navbar
   fluid={true}
   rounded={true}
-    className="sticky-top"
+    className="bg-white dark:bg-gray-900 fixed w-full z-20 top-0 left-0 border-b border-gray-200 dark:border-gray-600"
 >
     <Link href="/">
     <img
