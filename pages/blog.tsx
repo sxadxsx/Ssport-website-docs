a90789b723be8a647ec12af63cdf5d1f314721df
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-07 22:59:53 +0800

    Update blog.tsx

--- pages/blog.tsx
+++ pages/blog.tsx
@@ -3,7 +3,7 @@ import matter from 'gray-matter';
 import Image from 'next/image';
 import Link from 'next/link';
 
-export default function Blog({posts}) {
+export default function Blog {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -48,4 +48,4 @@ export default function Blog({posts}) {
   );
 }
 
-export default Blog({posts});
+export default Blog ;
