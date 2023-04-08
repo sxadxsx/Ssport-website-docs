048a77c6a43ecbda142e054a477929ab3ff57eb6
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-08 17:42:55 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -27,7 +27,8 @@ export default function Blog({posts}){
             const {title, author, category, date, bannerImage, tags} = frontmatter
 
             //JSX for individual blog listing
-            return <div className="grid gap-8 lg:grid-cols-2">
+            return <article key={title}>
+           <div className="grid gap-8 lg:grid-cols-2">
   <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div className="flex justify-between items-center mb-5 text-gray-500">
       <span className="bg-primary-100 text-primary-800 text-xs font-medium inline-flex items-center px-2.5 py-0.5 rounded dark:bg-primary-200 dark:text-primary-800">
