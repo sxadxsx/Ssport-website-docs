332500c3097c68f19b326d89d1319a7478f542e0
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-08 22:38:07 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -8,7 +8,12 @@ export default function Post({frontmatter, content}) {
 
     const {title, author, category, date, bannerImage, tags} = frontmatter
 
-    return <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+    return 
+    <NextSeo
+        title="{title}-雙龍體育blog"
+        description="{title}"
+      />
+    <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
       <header className="mb-4 lg:mb-6 not-format">
@@ -50,10 +55,7 @@ export default function Post({frontmatter, content}) {
       </header>
        <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
     </article>
-<NextSeo
-        title="{title}-雙龍體育blog"
-        description="{title}"
-      />
+
   </div>
 </main>
 
