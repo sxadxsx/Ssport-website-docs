249366ca9fcd4cfd7147956a5cbc7a21ed1fdff7
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-08 22:28:04 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -10,11 +10,10 @@ export default function Blog({posts}){
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
       <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        Our Blog
+        雙龍體育部落格
       </h2>
       <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-        We use an agile approach to test assumptions and connect with the needs
-        of your audience early and often.
+   任何關於雙龍體育的任何關於雙龍體育的消息和公告
       </p>
     </div>
   </div>
@@ -54,16 +53,16 @@ export default function Blog({posts}){
       <div className="flex items-center space-x-4">
         <img
           className="w-7 h-7 rounded-full"
-          src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/avatars/jese-leos.png"
-          alt="Jese Leos avatar"
+          src="https://discuss.ssangyongsports.org/data/avatars/l/0/1.jpg?1679114793"
+          alt="Peter yang"
         />
         <span className="font-medium dark:text-white">Jese Leos</span>
       </div>
       <Link
-        href="/blog/${slug}"
+        href={`/blog/${slug}`}
         className="inline-flex items-center font-medium text-primary-600 dark:text-primary-500 hover:underline"
       >
-        Read more
+        閱讀文章
         <svg
           className="ml-2 w-4 h-4"
           fill="currentColor"
