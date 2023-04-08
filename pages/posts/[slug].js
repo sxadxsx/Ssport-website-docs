de3a6dab263cf94259fdf877d866fe283cf09646
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-08 17:16:55 +0800

    Update [slug].js

--- pages/posts/[slug].js
+++ pages/posts/[slug].js
@@ -7,13 +7,52 @@ export default function Post({frontmatter, content}) {
 
     const {title, author, category, date, bannerImage, tags} = frontmatter
 
-    return <main>
-        <img src={bannerImage}/>
-        <h1>{title}</h1>
-        <h2>{author} || {date}</h2>
-        <h3>{category} || {tags.join()}</h3>
-        <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
-    </main>
+    return <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+  <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
+    <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+      <header className="mb-4 lg:mb-6 not-format">
+        <address className="flex items-center mb-6 not-italic">
+          <div className="inline-flex items-center mr-3 text-sm text-gray-900 dark:text-white">
+          <img src={bannerImage}/>
+          <h1 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">
+        {title}
+        </h1>
+            <img
+              className="mr-4 w-16 h-16 rounded-full"
+              src="https://flowbite.com/docs/images/people/profile-picture-2.jpg"
+              alt="Jese Leos"
+            />
+            <div>
+              <a
+                href="#"
+                rel="author"
+                className="text-xl font-bold text-gray-900 dark:text-white"
+              >
+                {author}
+              </a>
+              <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                Graphic Designer, educator &amp; CEO Flowbite
+              </p>
+              <p className="text-base font-light text-gray-500 dark:text-gray-400">
+                <time
+                  pubdate=""
+                  dateTime="2022-02-08"
+                  title="February 8th, 2022"
+                >
+                  Feb. 8, 2022
+                </time>
+              </p>
+            </div>
+          </div>
+        </address>
+      </header>
+      <p className="lead">
+      <div dangerouslySetInnerHTML={{ __html: md().render(content) }} />
+      </p>
+    </article>
+  </div>
+</main>
+
 }
 
 // Generating the paths for each post
