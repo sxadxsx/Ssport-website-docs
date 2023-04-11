b5be416b7b482a28cb1cbc8b90f5486cf0140389
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-11 18:29:08 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -62,30 +62,25 @@ export default function Post({frontmatter, content}) {
 
 // Generating the paths for each post
 export async function getStaticPaths() {
-  // Get list of all files from our posts directory
   const files = fs.readdirSync("posts");
-  // Generate a path for each one
   const paths = files.map((fileName) => ({
     params: {
       slug: fileName.replace(".md", ""),
     },
   }));
-  // return list of paths
   return {
     paths,
     fallback: false,
   };
 }
 
-
-// Generate the static props for the page
 export async function getStaticProps({ params: { slug } }) {
-    const fileName = fs.readFileSync(`posts/${slug}.md`, 'utf-8');
-    const { data: frontmatter, content } = matter(fileName);
-    return {
-      props: {
-        frontmatter,
-        content,
-      },
-    };
-  }
+  const fileName = fs.readFileSync(`posts/${slug}.md`, "utf-8");
+  const { data: frontmatter, content } = matter(fileName);
+  return {
+    props: {
+      frontmatter,
+      content,
+    },
+  };
+}
