a3345e3a61536875faba409b01f6c63385234bb2
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-11 21:57:35 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -1,6 +1,7 @@
 import fs from "fs";
 import matter from "gray-matter";
 import MarkdownIt from "markdown-it"; // import the markdown-it library
+import md from "markdown-it"; // import the markdown-it library
 import { NextSeo } from 'next-seo';
 import path from "path";
 
