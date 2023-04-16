b345b4a2209731206017a612a9b682fe947f0665
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-16 15:29:34 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -11,10 +11,9 @@ export default function Post({frontmatter, content}) {
 
     return <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
-    <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
       </br>
-        <header className="mb-4 lg:mb-6 not-format">
-                   
+    <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
+        <header className="mb-4 lg:mb-6 not-format">                   
 <img
   className="h-auto max-w-lg rounded-lg max-w-full"
   src={bannerImage}
