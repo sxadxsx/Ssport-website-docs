a708defe53c7cdf6a772727e23134b922e1f64b6
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-18 18:50:21 +0800

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -7,33 +7,6 @@ function footer() {
     <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
       <div>
         <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-          Company
-        </h2>
-        <ul className="text-gray-500 dark:text-gray-400">
-          <li className="mb-4">
-            <a href="#" className=" hover:underline">
-              About
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Careers
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Brand Center
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Blog
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
           Help center
         </h2>
         <ul className="text-gray-500 dark:text-gray-400">
