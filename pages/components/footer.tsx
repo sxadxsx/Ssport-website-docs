123f88b409df12edf9717e41cce4b550aaaffd27
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-18 19:22:05 +0800

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -7,71 +7,88 @@ function footer() {
     <div className="grid grid-cols-2 gap-8 md:grid-cols-3 lg:grid-cols-5">
       <div>
         <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-          Help center
-        </h2>
-        <ul className="text-gray-500 dark:text-gray-400">
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Discord Server
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Twitter
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Facebook
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Contact Us
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-          Legal
-        </h2>
-        <ul className="text-gray-500 dark:text-gray-400">
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Privacy Policy
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Licensing
-            </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Terms
-            </a>
-          </li>
-        </ul>
-      </div>
-      <div>
-        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-          Company
+          組織
         </h2>
         <ul className="text-gray-500 dark:text-gray-400">
           <li className="mb-4">
             <a href="#" className=" hover:underline">
-              About
+              關於
             </a>
           </li>
           <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Careers
+            <Link href="/blog" className="hover:underline">
+              部落格
+            </Link>
+          </li>
+          <li className="mb-4">
+            <Link href="c" className="hover:underline">
+             聯繫
+            </Link>
+           </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+          幫助
+        </h2>
+        <ul className="text-gray-500 dark:text-gray-400">
+          <li className="mb-4">
+            <a href="https://help.ssangyongsports.org/" className="hover:underline">
+             幫助中心
             </a>
           </li>
           <li className="mb-4">
-            <a href="#" className="hover:underline">
-              Brand Center
+            <a href="https://help.ssangyongsports.org/" className="hover:underline">
+            線上客服
+            </a>
+          </li>
+          <li className="mb-4">
+            <a href="https://discuss.ssangyongsports.org/" className="hover:underline">
+              論壇
+            </a>
+          </li>
+          <li className="mb-4">
+            <Link href="c" className="hover:underline">
+              聯繫
+            </Link>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+          條款
+        </h2>
+        <ul className="text-gray-500 dark:text-gray-400">
+          <li className="mb-4">
+            <Link href="/p" className="hover:underline">
+              隱私政策
+            </a>
+          </li>
+          <li className="mb-4">
+            <a href="/t" className="hover:underline">
+              服務條款
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div>
+        <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
+          雙龍體育TV
+        </h2>
+        <ul className="text-gray-500 dark:text-gray-400">
+          <li className="mb-4">
+            <Link href="/tv" className=" hover:underline">
+              官網
+            </Link>
+          </li>
+          <li className="mb-4">
+            <a href="https://tv.ssangyongsports.org/buy.html" className="hover:underline">
+              註冊
+            </a>
+          </li>
+          <li className="mb-4">
+            <a href="https://tv.ssangyongsports.org/login" className="hover:underline">
+             登入
             </a>
           </li>
           <li className="mb-4">
@@ -83,36 +100,31 @@ function footer() {
       </div>
       <div>
         <h2 className="mb-6 text-sm font-semibold text-gray-900 uppercase dark:text-white">
-          Download
+          體育
         </h2>
         <ul className="text-gray-500 dark:text-gray-400">
           <li className="mb-4">
             <a href="#" className="hover:underline">
-              iOS
+              雙龍職棒
             </a>
           </li>
           <li className="mb-4">
             <a href="#" className="hover:underline">
-              Android
+              雙龍職籃
             </a>
           </li>
           <li className="mb-4">
             <a href="#" className="hover:underline">
-              Windows
+              雙龍足球
             </a>
-          </li>
-          <li className="mb-4">
-            <a href="#" className="hover:underline">
-              MacOS
-            </a>
-          </li>
+          </li>          
         </ul>
       </div>
     </div>
     <hr className="my-6 border-gray-200 sm:mx-auto dark:border-gray-700 lg:my-8" />
     <div className="text-center">
-      <a
-        href="#"
+      <Link
+        href="/"
         className="flex justify-center items-center mb-5 text-2xl font-semibold text-gray-900 dark:text-white"
       >
         <img
@@ -120,7 +132,7 @@ function footer() {
   width={186}
   height={46}
 />
-</a>
+</Link>
       <span className="block text-sm text-center text-gray-500 dark:text-gray-400">
         © 2021-2022{" "}
         <a href="#" className="hover:underline">
@@ -131,7 +143,7 @@ function footer() {
       <ul className="flex justify-center mt-5 space-x-5">
         <li>
           <a
-            href="#"
+            href="https://www.facebook.com/ssangyongsports/"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
           >
             <svg
@@ -147,29 +159,10 @@ function footer() {
               />
             </svg>
           </a>
-        </li>
+        </li>        
         <li>
           <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
-          >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12.315 2c2.43 0 2.784.013 3.808.06 1.064.049 1.791.218 2.427.465a4.902 4.902 0 011.772 1.153 4.902 4.902 0 011.153 1.772c.247.636.416 1.363.465 2.427.048 1.067.06 1.407.06 4.123v.08c0 2.643-.012 2.987-.06 4.043-.049 1.064-.218 1.791-.465 2.427a4.902 4.902 0 01-1.153 1.772 4.902 4.902 0 01-1.772 1.153c-.636.247-1.363.416-2.427.465-1.067.048-1.407.06-4.123.06h-.08c-2.643 0-2.987-.012-4.043-.06-1.064-.049-1.791-.218-2.427-.465a4.902 4.902 0 01-1.772-1.153 4.902 4.902 0 01-1.153-1.772c-.247-.636-.416-1.363-.465-2.427-.047-1.024-.06-1.379-.06-3.808v-.63c0-2.43.013-2.784.06-3.808.049-1.064.218-1.791.465-2.427a4.902 4.902 0 011.153-1.772A4.902 4.902 0 015.45 2.525c.636-.247 1.363-.416 2.427-.465C8.901 2.013 9.256 2 11.685 2h.63zm-.081 1.802h-.468c-2.456 0-2.784.011-3.807.058-.975.045-1.504.207-1.857.344-.467.182-.8.398-1.15.748-.35.35-.566.683-.748 1.15-.137.353-.3.882-.344 1.857-.047 1.023-.058 1.351-.058 3.807v.468c0 2.456.011 2.784.058 3.807.045.975.207 1.504.344 1.857.182.466.399.8.748 1.15.35.35.683.566 1.15.748.353.137.882.3 1.857.344 1.054.048 1.37.058 4.041.058h.08c2.597 0 2.917-.01 3.96-.058.976-.045 1.505-.207 1.858-.344.466-.182.8-.398 1.15-.748.35-.35.566-.683.748-1.15.137-.353.3-.882.344-1.857.048-1.055.058-1.37.058-4.041v-.08c0-2.597-.01-2.917-.058-3.96-.045-.976-.207-1.505-.344-1.858a3.097 3.097 0 00-.748-1.15 3.098 3.098 0 00-1.15-.748c-.353-.137-.882-.3-1.857-.344-1.023-.047-1.351-.058-3.807-.058zM12 6.865a5.135 5.135 0 110 10.27 5.135 5.135 0 010-10.27zm0 1.802a3.333 3.333 0 100 6.666 3.333 3.333 0 000-6.666zm5.338-3.205a1.2 1.2 0 110 2.4 1.2 1.2 0 010-2.4z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
-        <li>
-          <a
-            href="#"
+            href="https://twitter.com/ssangyongsport"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
           >
             <svg
@@ -184,7 +177,7 @@ function footer() {
         </li>
         <li>
           <a
-            href="#"
+            href="https://github.com/ssangyongsportsorg"
             className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
           >
             <svg
@@ -200,26 +193,7 @@ function footer() {
               />
             </svg>
           </a>
-        </li>
-        <li>
-          <a
-            href="#"
-            className="text-gray-500 hover:text-gray-900 dark:hover:text-white dark:text-gray-400"
-          >
-            <svg
-              className="w-5 h-5"
-              fill="currentColor"
-              viewBox="0 0 24 24"
-              aria-hidden="true"
-            >
-              <path
-                fillRule="evenodd"
-                d="M12 2C6.48 2 2 6.48 2 12s4.48 10 10 10c5.51 0 10-4.48 10-10S17.51 2 12 2zm6.605 4.61a8.502 8.502 0 011.93 5.314c-.281-.054-3.101-.629-5.943-.271-.065-.141-.12-.293-.184-.445a25.416 25.416 0 00-.564-1.236c3.145-1.28 4.577-3.124 4.761-3.362zM12 3.475c2.17 0 4.154.813 5.662 2.148-.152.216-1.443 1.941-4.48 3.08-1.399-2.57-2.95-4.675-3.189-5A8.687 8.687 0 0112 3.475zm-3.633.803a53.896 53.896 0 013.167 4.935c-3.992 1.063-7.517 1.04-7.896 1.04a8.581 8.581 0 014.729-5.975zM3.453 12.01v-.26c.37.01 4.512.065 8.775-1.215.25.477.477.965.694 1.453-.109.033-.228.065-.336.098-4.404 1.42-6.747 5.303-6.942 5.629a8.522 8.522 0 01-2.19-5.705zM12 20.547a8.482 8.482 0 01-5.239-1.8c.152-.315 1.888-3.656 6.703-5.337.022-.01.033-.01.054-.022a35.318 35.318 0 011.823 6.475 8.4 8.4 0 01-3.341.684zm4.761-1.465c-.086-.52-.542-3.015-1.659-6.084 2.679-.423 5.022.271 5.314.369a8.468 8.468 0 01-3.655 5.715z"
-                clipRule="evenodd"
-              />
-            </svg>
-          </a>
-        </li>
+        </li>       
       </ul>
     </div>
   </div>
@@ -228,4 +202,3 @@ function footer() {
 }
 
 export default footer;
-
