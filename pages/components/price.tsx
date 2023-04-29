5d892b873eed3189b23442fb7e4221ccb886d18f
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-04-29 23:12:59 +0800

    Update price.tsx

--- pages/components/price.tsx
+++ pages/components/price.tsx
@@ -6,21 +6,20 @@ function price() {
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-md text-center mb-8 lg:mb-12">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        Designed for business teams like yours
+        選擇適合你的價格
       </h2>
       <p className="mb-5 font-light text-gray-500 sm:text-xl dark:text-gray-400">
-        Here at Flowbite we focus on markets where technology, innovation, and
-        capital can unlock long-term value and drive economic growth.
+       這裡提供了價格給你參考
       </p>
     </div>
     <div className="space-y-8 lg:grid lg:grid-cols-3 sm:gap-6 xl:gap-10 lg:space-y-0">
       <div className="flex flex-col p-6 mx-auto max-w-lg text-center text-gray-900 bg-white rounded-lg border border-gray-100 shadow dark:border-gray-600 xl:p-8 dark:bg-gray-800 dark:text-white">
-        <h3 className="mb-4 text-2xl font-semibold">Starter</h3>
+        <h3 className="mb-4 text-2xl font-semibold">基本計畫</h3>
         <p className="font-light text-gray-500 sm:text-lg dark:text-gray-400">
-          Best option for personal use &amp; for your next project.
+         半年繳
         </p>
         <div className="flex justify-center items-baseline my-8">
-          <span className="mr-2 text-5xl font-extrabold">$29</span>
+          <span className="mr-2 text-5xl font-extrabold">$500</span>
           <span className="text-gray-500 dark:text-gray-400">/month</span>
         </div>
         <ul role="list" className="mb-8 space-y-4 text-left">
@@ -37,7 +36,7 @@ function price() {
                 clipRule="evenodd"
               />
             </svg>
-            <span>Individual configuration</span>
+            <span>cdn加強</span>
           </li>
           <li className="flex items-center space-x-3">
             <svg
@@ -52,7 +51,7 @@ function price() {
                 clipRule="evenodd"
               />
             </svg>
-            <span>No setup, or hidden fees</span>
+            <span>不出賣數據</span>
           </li>
           <li className="flex items-center space-x-3">
             <svg
@@ -68,7 +67,7 @@ function price() {
               />
             </svg>
             <span>
-              Team size: <span className="font-semibold">1 developer</span>
+             同時共用人數: <span className="font-semibold">3</span>
             </span>
           </li>
           <li className="flex items-center space-x-3">
@@ -85,26 +84,10 @@ function price() {
               />
             </svg>
             <span>
-              Premium support: <span className="font-semibold">6 months</span>
+              高級幫助: <span className="font-semibold">3個月</span>
             </span>
           </li>
-          <li className="flex items-center space-x-3">
-            <svg
-              className="flex-shrink-0 w-5 h-5 text-green-500 dark:text-green-400"
-              fill="currentColor"
-              viewBox="0 0 20 20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                fillRule="evenodd"
-                d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z"
-                clipRule="evenodd"
-              />
-            </svg>
-            <span>
-              Free updates: <span className="font-semibold">6 months</span>
-            </span>
-          </li>
+          
         </ul>
         <a
           href="#"
