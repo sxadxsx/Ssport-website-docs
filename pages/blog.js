2bc6d0b23298828af1a4c2494994d0810ca46547
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-04 21:18:28 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -7,6 +7,7 @@ import { NextSeo } from 'next-seo';
 // The Blog Page Content
 export default function Blog({posts}){
     return <main>
+        <>
         <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
@@ -60,7 +61,7 @@ export default function Blog({posts}){
         })}
 </div>
               </article> 
-
+</>
     </main>
 }
 
