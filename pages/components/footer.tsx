fef710e97233daaed335b458966829a284abba09
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-05 17:23:00 +0800

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -1,22 +1,19 @@
 import Link from 'next/link';
+function updateFooterBadge(status) {
+  const badge = document.getElementById('footer-badge');
 
-const FooterBadge = () => {
+  badge.textContent = status === 'UP' ? '正常運行' : '異常';
+  badge.className = status.toLowerCase();
+  badge.href = 'https://status.ssangyongsports.org';
+}
+function FooterBadge() {
   useEffect(() => {
-    // 定義一個函數來更新Footer badge的內容和樣式
-    function updateFooterBadge(status) {
-      const badge = document.getElementById('footer-badge');
-
-      badge.textContent = status === 'UP' ? '正常運行' : '異常';
-      badge.className = status.toLowerCase();
-    }
-
     // 從您的JSON代碼中讀取系統狀態並更新Footer badge
     fetch('https://ssangyongsports.instatus.com/summary.json')
       .then(response => response.json())
       .then(data => {
         const status = data.page.status;
         updateFooterBadge(status);
-        badge.href = data.page.url;
       })
       .catch(error => {
         console.error('無法讀取系統狀態', error);
