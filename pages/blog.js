1767df41fb5b59b8b2b2faced9febba4fc414bb6
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-05 21:33:13 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -7,22 +7,12 @@ import { NextSeo } from 'next-seo';
 // The Blog Page Content
 export default function Blog({posts}){
     return <main>
-        <section className="bg-white dark:bg-gray-900">
-  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-      <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        雙龍體育部落格
-      </h2>
-      <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
-   任何關於雙龍體育的任何關於雙龍體育的消息和公告
-      </p>
+       
     <NextSeo
         title="雙龍體育blog"
         description="任何關於雙龍體育的任何關於雙龍體育的消息和公告,就上雙龍體育blog"
       />
-    </div>
-  </div>
-</section>
+
 
         {posts.map(post => {
             //extract slug and frontmatter
@@ -33,8 +23,15 @@ export default function Blog({posts}){
             //JSX for individual blog listing
             return 
             <article key={title}>
-                                 <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-                        <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+                 <section className="bg-white dark:bg-gray-900">
+  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+    <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
+      <h2 className="mb-4 text-3xl lg:text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+        雙龍體育部落格
+      </h2>
+      <p className="font-light text-gray-500 sm:text-xl dark:text-gray-400">
+   任何關於雙龍體育的任何關於雙龍體育的消息和公告
+      </p>
            <div className="grid gap-8 lg:grid-cols-2">
   <article className="p-6 bg-white rounded-lg border border-gray-200 shadow-md dark:bg-gray-800 dark:border-gray-700">
     <div className="flex justify-between items-center mb-5 text-gray-500">
