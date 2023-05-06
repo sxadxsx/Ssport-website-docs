17dff1a465ecde541e5522c5606f685f422de82e
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-06 23:20:44 +0800

    Update Hero.tsx

--- pages/components/Hero.tsx
+++ pages/components/Hero.tsx
@@ -38,8 +38,8 @@ function hero() {
      體育組織
     </h2>
     <div className="flex flex-col mb-8 lg:mb-16 space-y-4 sm:flex-row sm:justify-center sm:space-y-0 sm:space-x-4">
-      <a
-        href="#"
+      <Link
+        href="#anchor_one"
         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-white rounded-lg bg-primary-700 hover:bg-primary-800 focus:ring-4 focus:ring-primary-300 dark:focus:ring-primary-900"
       >
         Learn more
@@ -55,7 +55,7 @@ function hero() {
             clipRule="evenodd"
           />
         </svg>
-      </a>
+      </Link>
       <a
         href="#"
         className="inline-flex justify-center items-center py-3 px-5 text-base font-medium text-center text-gray-900 rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:ring-gray-100 dark:text-white dark:border-gray-700 dark:hover:bg-gray-700 dark:focus:ring-gray-800"
