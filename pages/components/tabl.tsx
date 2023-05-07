c4f0407a86798e67903872c9aeab2ed01e1a953e
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-07 10:53:35 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -1,12 +1,13 @@
 import Link from 'next/link';
 import Image from 'next/image';
+import styles from './Button.module.css';
 
 function Tabl() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <div className="mt-4 md:mt-0">
-          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
+          <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white {styles.abc}">
             雙龍職籃
           </h2>
           <p className="mb-6 font-light text-gray-500 md:text-lg dark:text-gray-400">
