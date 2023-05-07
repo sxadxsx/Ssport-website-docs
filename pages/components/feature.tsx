b94e4aab109a0a009928ae00bc5d9bd9e58ba6ab
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-07 22:40:13 +0800

    Update feature.tsx

--- pages/components/feature.tsx
+++ pages/components/feature.tsx
@@ -6,8 +6,7 @@ return (
     <div className="max-w-screen-md mb-8 lg:mb-16">
       <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
         優點及
-      </h2><h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white ${styles.abc}`}>
-        好處
+      <span className={styles.abc}>好處</span>
       </h2>
       <p className="text-gray-500 sm:text-xl dark:text-gray-400">
        提供了使用的優點
