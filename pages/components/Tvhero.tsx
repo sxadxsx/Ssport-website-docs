5af40a6ef9426e07585316fb6fa87f7b32ac1503
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-07 22:34:37 +0800

    Update Tvhero.tsx

--- pages/components/Tvhero.tsx
+++ pages/components/Tvhero.tsx
@@ -1,11 +1,11 @@
 import Link from 'next/link';
-
+import styles from './Button.module.css';
 function tvhero() {
   return (
     <section className="bg-white dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white">
+          <h1 className={`max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white ${styles.abc}`}>
             歡迎加入雙龍體育TV
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
