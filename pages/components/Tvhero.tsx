a4f64f31fa8102e2d05d231233bf776dc9af5d05
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-09 07:13:45 +0800

    Update Tvhero.tsx

--- pages/components/Tvhero.tsx
+++ pages/components/Tvhero.tsx
@@ -5,8 +5,9 @@ function tvhero() {
     <section className="bg-white dark:bg-gray-900">
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="mr-auto place-self-center lg:col-span-7">
-          <h1 className={`max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white ${styles.abc}`}>
-            歡迎加入雙龍體育TV
+          <h1 className={`max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl dark:text-white`}>
+            歡迎加入
+                 <span className={styles.abc}>雙龍體育TV</span>
           </h1>
           <p className="max-w-2xl mb-6 font-light text-gray-500 lg:mb-8 md:text-lg lg:text-xl dark:text-gray-400">
             雙龍體育直播比賽，快速，安全 立刻購買！
