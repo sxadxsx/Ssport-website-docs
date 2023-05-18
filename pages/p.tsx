48d6fc471f30a7621fd5356f3cd75321a0f707eb
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-18 22:12:42 +0800

    Update p.tsx

--- pages/p.tsx
+++ pages/p.tsx
@@ -1,5 +1,4 @@
-import { NextSeo } from 'next-seo';
-
+import Head from 'next/head'
 const p = () => {
   return (
 
@@ -120,11 +119,11 @@ const p = () => {
     <br />
   </p>
   <p>support@ssangyongsport.org 雙龍體育團隊</p>
-
-  <NextSeo
-        title="隱私政策-雙龍體育"
-        description=" 雙龍體育的政策是尊重您的隱私並遵守有關我們可能收集的關於您的任何個人信息的任何適用法律和法規，包括我們的網站ssangyongsports.org 以及我們擁有和."
-      />
+<Head>
+        <title>隱私政策-雙龍體育</title>
+        <meta property="og:title" content="雙龍體育的政策是尊重您的隱私並遵守有關我們可能收集的關於您的任何個人信息的任何適用法律和法規，包括我們的網站ssangyongsports.org 以及我們擁有和." key="title" />
+      </Head>
+  
       </header>
     </article> 
   </div>
