6da8916655da4b888dcb7424813388392432b185
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-18 22:13:35 +0800

    Update tv.tsx

--- pages/tv.tsx
+++ pages/tv.tsx
@@ -2,14 +2,14 @@ import Tvhero from './components/Tvhero';
 import Price from './components/price';
 import Feature from './components/feature';
 import React from 'react';
-import { NextSeo } from 'next-seo';
+import Head from 'next/head'
 const Home = () => {
   return (
     <>
-      <NextSeo
-        title="雙龍體育TV"
-        description="雙龍體育TV直播比賽，快速，安全立刻購買！ 註冊登入"
-      />
+<Head>
+        <title>雙龍體育TV</title>
+        <meta property="og:title" content="雙龍體育TV直播比賽，快速，安全立刻購買！ 註冊登入" key="title" />
+      </Head>
       <div>
         <Tvhero />
         <Feature />
