5d034d242ca0d2aefddf83079b1657dc1e09ef87
Peter yang <92738287+ssangyongsports@users.noreply.github.com>
2023-05-19 21:11:04 +0800

    Create t.tsx

---
+++ pages/t.tsx
@@ -0,0 +1,22 @@
+import Tvhero from './components/Tvhero';
+import Price from './components/price';
+import Feature from './components/feature';
+import React from 'react';
+import Head from 'next/head'
+const t = () => {
+  return (
+    <>
+<Head>
+        <title>服務條款-雙龍體育</title>
+        <meta name="description" content="歡迎加入雙龍體育組織！在您註冊成為雙龍體育組織的會員之前，請您仔細閱讀以下服務條款。註冊成為會員表示您已經閱讀、理解並同意遵守本條款。" />
+      </Head>
+      <div>
+           <h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">服務條款</h2>
+<div>歡迎加入雙龍體育組織！在您註冊成為雙龍體育組織的會員之前，請您仔細閱讀以下服務條款。註冊成為會員表示您已經閱讀、理解並同意遵守本條款。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">1. 會員資格</h2><div>雙龍體育組織歡迎任何年齡、性別、種族和國籍的人士加入。註冊成為會員不受年齡限制，但如果您未滿18歲，您需要在家長或監護人的同意下註冊和使用本組織提供的服務。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">2. 註冊資料與資料保護</h2><div>在註冊過程中，您需要提供有效的電子郵件地址、姓名、出生日期等個人資料。您同意提供真實、準確、完整並及時更新的資料，以便我們為您提供更好的服務。雙龍體育組織將尊重並保護您的隱私，並採取適當的技術措施來保護您的個人資料。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">3. 會員帳號與密碼</h2><div>註冊成功後，您將收到一個會員帳號和密碼。您有責任保管好您的帳號和密碼，並對您的帳號進行的所有活動負責。如果您發現帳號被盜用或存在安全漏洞，請立即聯繫我們：support@ssangyongsports.org。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">4. 服務範圍</h2><div>雙龍體育組織提供多種體育活動、課程、賽事報名等服務。部分活動和課程可能需要支付額外費用，具體費用和支付方式請參照相關活動和課程介紹。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">5. 會員行為規範</h2><div>作為雙龍體育組織的會員，您同意遵守以下行為規範：</div><div><br /></div><div>遵守相關法律法規；</div><div>尊重他人的權利和感受，不進行歧視、侮辱、挑釁等不良行為；</div><div>正確使用並保護組織提供的設施和器材；</div><div>不在組織活動中從事任何非法、違規或不道德的行為。</div><div>違反上述規定的會員，雙龍體育組織有權採取警告、暫停或終止會員資格等措施。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">6. 免責聲明</h2><div>雖然雙龍體育組織會努力為您提供高品質的服務，但我們無法保證所有活動和課程都能滿足您的需求和期望。您應自行承擔參加活動和課程的風險，雙龍體育組織對於因您個人原因造成的任何損失、傷害或不滿不承擔責任。在法律允許的範圍內，雙龍體育組織對本服務條款所涉及的所有直接或間接損害不承擔責任。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">7. 條款修改</h2><div>雙龍體育組織保留隨時修改本服務條款的權利。一旦本條款發生變更，我們將在網站上公告，並將變更後的條款發送至您註冊時提供的電子郵件地址。您有責任定期查看本服務條款。繼續使用雙龍體育組織的服務將被視為您接受修改後的條款。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">8. 準據法與管轄權</h2><div>本服務條款的解釋和適用以及與本服務條款有關的爭議均適用於中華民國法律。您同意，如與雙龍體育組織之間發生任何爭議，應首先協商解決，協商不成的，雙方一致同意將爭議提交至中華民國台灣地區法院管轄。</div><div><br /></div><h2 className="mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white">9. 聯繫方式</h2><div>如果您對本服務條款或雙龍體育組織的服務有任何疑問、建議或投訴，請通過以下方式聯繫我們：</div><div><br /></div><div>電子郵件：support@ssangyongsports.org</div><div><br /></div><div>我們將在收到您的郵件後，盡快予以回應和處理。</div><div><br /></div><div>感謝您選擇成為雙龍體育組織的會員，祝您在我們的組織中度過愉快的時光！</div>
+    
+      </div>
+    </>
+  );
+}
+
+export default t;
