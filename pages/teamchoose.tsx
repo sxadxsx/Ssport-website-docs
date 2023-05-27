39ec5e1bad4aa05f0294800f07fe42a81f1769af
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-05-28 00:09:10 +0800

    Update teamchoose.tsx

--- pages/teamchoose.tsx
+++ pages/teamchoose.tsx
@@ -65,6 +65,31 @@ function teamchoose() {
         >
 
           球隊1
+          <label
+
+          htmlFor="subject"
+
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+
+        >
+
+          主旨
+
+        </label>
+
+        <input
+
+          type="text"
+
+          id="subject"
+
+          required={true}
+
+          className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+
+          placeholder="讓我們得知如何幫助你"
+
+          />
 
         </label>
 <label
@@ -76,7 +101,32 @@ function teamchoose() {
         >
 
           球隊2
+  
+<label
 
+          htmlFor="subject"
+
+          className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
+
+        >
+
+          主旨
+
+        </label>
+
+        <input
+
+          type="text"
+
+          id="subject"
+
+          required={true}
+
+          className="block p-3 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 shadow-sm focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
+
+          placeholder="讓我們得知如何幫助你"
+
+          />
         </label>
         <label
 
