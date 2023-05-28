5e06949ac4e92479f0bae44df4d644361ab278db
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-05-28 19:42:27 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -33,7 +33,7 @@ function c() {
 
 <input
   type="email"
-  id="email"
+  name="email"
   className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
   placeholder="peter@ssangyongsports.org"
   required={true}
