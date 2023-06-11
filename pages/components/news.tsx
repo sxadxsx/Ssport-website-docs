d353433e711cd9bc2eccd09edaacd96c66c08a55
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-11 22:32:49 +0800

    Update news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -31,12 +31,7 @@ function news() {
               </svg>
             </div>
            <input
-  className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-  placeholder="輸入您的電子郵件"
-  type="email"
-  id="email"
-  name="EMAIL"
-/>
+               <input className="input" type="text" id="EMAIL" name="EMAIL" autoComplete="off" placeholder="EMAIL" required />
           </div>
           <div>
             <button
