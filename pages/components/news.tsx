871ffd75c8af08827c0cc81c0cff0ce99b7c46e1
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-13 20:08:16 +0800

    Update news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -32,10 +32,12 @@ function news() {
             </div>
               <input
   className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
-  placeholder="輸入您的電子郵件"
-  type="email"
-  id="email"
+  type="text"
+  id="EMAIL"
   name="EMAIL"
+  autoComplete="off"
+  placeholder="EMAIL"
+  data-required="true"
 />
           </div>
           <div>
