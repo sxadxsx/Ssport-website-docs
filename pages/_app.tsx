771a7d9785eb6c5e5a1ef2f0621b24555284e6bf
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-14 22:49:11 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,12 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
-import News from './components/news'
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
     <Head />
 <Component {...pageProps} />
-          <News />
     <Footer />
   </div>
 
