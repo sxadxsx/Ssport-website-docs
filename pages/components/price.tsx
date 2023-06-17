a172acf92fb7a0f750608c248ed0f5bb48d4bc80
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-17 22:40:38 +0800

    Update price.tsx

--- pages/components/price.tsx
+++ pages/components/price.tsx
@@ -92,7 +92,7 @@ function price() {
           
         </ul>
         <Link
-          herf="/c"
+          href="/c"
           className="text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:ring-primary-200 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:text-white  dark:focus:ring-primary-900"
         >
           聯繫我們
