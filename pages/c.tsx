1fb7bca638df35fa62a6f5a678fc34aa96aa940e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-23 12:08:25 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -92,6 +92,9 @@ function c() {
         />
           </label>
       
+<label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="multiple_files">上傳文件</label>
+<input class="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400" id="multiple_files" type="file" multiple>
+
       <button
         type="submit"
         
