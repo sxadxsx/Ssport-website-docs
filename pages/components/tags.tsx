a7f20304e3ba16e9a1b6db6f583511cb38f87dcd
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-01 00:05:21 +0800

    Create tags.tsx

---
+++ pages/components/tags.tsx
@@ -0,0 +1,99 @@
+import styles from './Button.module.css';
+function tags() {
+return (
+<section class="bg-white dark:bg-gray-900 antialiased">
+  <div class="max-w-screen-xl px-4 py-8 mx-auto lg:px-6 sm:py-16 lg:py-24">
+    <div class="max-w-2xl mx-auto text-center">
+      <h2 class="text-3xl font-extrabold leading-tight tracking-tight text-gray-900 sm:text-4xl dark:text-white">
+        我們的體育
+      </h2>
+      <p class="mt-4 text-base font-normal text-gray-500 sm:text-xl dark:text-gray-400">
+        Crafted with skill and care to help our clients grow their business!
+      </p>
+    </div>
+
+    <div class="grid grid-cols-1 mt-12 text-center sm:mt-16 gap-x-20 gap-y-12 sm:grid-cols-2 lg:grid-cols-3">
+      <div class="space-y-4">
+          <img
+              alt=""
+              className="service-icon"
+              src="https://74-rho.vercel.app/images/baseball-16.png"
+              width={75}
+            />
+        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+          雙龍職棒
+        </h3>
+        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+          棒球，（英語：Baseball、日韓稱為野球），為一種團體球類運動，由人數最少為9人的两支队伍在一個扇形的球場进行攻擊與守備。棒球球員分為攻...
+        </p>
+        <a href="https://slb.ssangyongsports.org/" title=""
+          class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          role="button">
+          View case study
+          <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clip-rule="evenodd" />
+          </svg>
+        </a>
+      </div>
+
+      <div class="space-y-4">
+       <img
+              alt=""
+              className="service-icon"
+              src="https://74-rho.vercel.app/images/889455.png"
+              width={75}
+            />
+        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+          雙龍職籃
+        </h3>
+        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+籃球，為一種由兩隊參與，在一個長方形籃球場進行的球類運動。每隊出場5名隊員，可將球向任何方向傳、投、拍、滾或運，目的是將籃球投入對方球籃得分，並阻止對方獲得控球權或得...
+        </p>
+        <a href="https://sba.ssangyongsports.org/" title=""
+          class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          role="button">
+          View case study
+          <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clip-rule="evenodd" />
+          </svg>
+        </a>
+      </div>
+
+      <div class="space-y-4">
+        <img
+              alt=""
+              className="service-icon"
+              src="https://74-rho.vercel.app/images/53283.png"
+              width={75}
+            />
+        <h3 class="text-2xl font-bold leading-tight text-gray-900 dark:text-white">
+雙龍足球
+        </h3>
+        <p class="text-lg font-normal text-gray-500 dark:text-gray-400">
+         足球（football）主要專指英式足球，官方名為協會足球（association football，簡稱soccer），是一種世界流行的團體球類運動，也是所有體育運動中最受歡迎、接受度最高、普及面最廣的一種，被譽為「世界遊戲」
+        </p>
+        <a href="#" title=""
+          class="text-white bg-primary-700 justify-center hover:bg-primary-800 inline-flex items-center  focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
+          role="button">
+          View case study
+          <svg aria-hidden="true" class="w-5 h-5 ml-2 -mr-1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"
+            fill="currentColor">
+            <path fill-rule="evenodd"
+              d="M12.293 5.293a1 1 0 011.414 0l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414-1.414L14.586 11H3a1 1 0 110-2h11.586l-2.293-2.293a1 1 0 010-1.414z"
+              clip-rule="evenodd" />
+          </svg>
+        </a>
+      </div>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default tags;
