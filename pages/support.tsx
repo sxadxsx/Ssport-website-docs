f45801de72431dda7ee8bca87ef246e1e8c342d2
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-06-30 21:59:42 +0800

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -1,8 +1,17 @@
 import Head from 'next/head'
-
 import styles from './components/Button.module.css';
+import { useState } from 'react';
+import { useRouter } from 'next/router';
 
-const support = () => {
+export default function Search() {
+  const [searchTerm, setSearchTerm] = useState('');
+  const router = useRouter();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    router.push(`https://help.ssangyongsports.org/search?q=${encodeURIComponent(searchTerm)}`);
+  };
+
   return (
         <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
@@ -34,18 +43,22 @@ const support = () => {
         />
       </svg>
     </div>
-    <input
-      type="search"
-      id="default-search"
-      className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-      placeholder="Search Mockups, Logos..."
-    />
+          <form onSubmit={handleSubmit}>
+
+ <input
+          type="text"
+          value={searchTerm}
+          onChange={(event) => setSearchTerm(event.target.value)}
+          placeholder="Search..."
+         className="block w-full p-4 pl-10 text-sm text-gray-900 border border-gray-300 rounded-lg bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+        />
     <button
       type="submit"
       className="text-white absolute right-2.5 bottom-2.5 bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-4 py-2 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
     >
       Search
     </button>
+            </form>
   </div>
       <Head>
         <title>雙龍體育blog</title>
