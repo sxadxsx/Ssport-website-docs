16ffb1a7a2fb85e2118aebc4bc18f3d6155f33d9
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-01 18:38:45 +0800

    Update c.tsx

--- pages/c.tsx
+++ pages/c.tsx
@@ -41,20 +41,8 @@ function c() {
   required={true}
 />
       </label>
-       <label
-         htmlFor="email"
-  className="block mb-2 text-sm font-medium text-gray-900 dark:text-gray-300"
->
-  你的名字
-
-<input
-  type="name"
-  name="_email.from"
-  className="shadow-sm bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500 dark:shadow-sm-light"
-  placeholder="peter"
-  required={true}
-/>
-      </label>
+      <input type="hidden" name="_email.subject" value="雙龍體育網站聯繫資訊" />
+       <input type="hidden" name="_email.from" value="雙龍體育聯繫客戶" />
       <div
       className="max-w-md"
       id="select"
