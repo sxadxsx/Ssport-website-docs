ff55b82a3fdcfbaf600b1312a821780bccca8891
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-01 23:43:38 +0800

    Update head.tsx

--- pages/components/head.tsx
+++ pages/components/head.tsx
@@ -34,9 +34,9 @@ function head() {
     >
       SLB
     </Navbar.Link>
-    <Navbar.Link href="https://sba.ssangyongsports.org">
+    <a href="https://sba.ssangyongsports.org" className="block py-2 pr-4 pl-3 md:p-0 border-b border-gray-100  text-gray-700 hover:bg-gray-50 dark:border-gray-700 dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:border-0 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:bg-transparent md:dark:hover:text-white">
       SBA
-    </Navbar.Link>
+    </a>
     <Navbar.Link>
     <Link href="/p" passHref={true}>
       隱私政策
