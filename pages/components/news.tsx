78b40c43f56e5f2fe48af6938f67db733d12a72b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-04 22:07:40 +0800

    更新 news.tsx

--- pages/components/news.tsx
+++ pages/components/news.tsx
@@ -34,7 +34,7 @@ function news() {
                         <input 
              type="hidden" 
              name="l" 
-             value="d1780fb9-0a82-421f-80bf-a87ee955890e" 
+          value="d1780fb9-0a82-421f-80bf-a87ee955890e" 
            />
             <input
               className="block p-3 pl-10 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 sm:rounded-none sm:rounded-l-lg focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
