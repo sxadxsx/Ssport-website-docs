c0440e92fec69a66faffc44aad8c644511121dde
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-05 18:32:26 +0800

    Update sport.tsx

--- pages/components/sport.tsx
+++ pages/components/sport.tsx
@@ -12,71 +12,7 @@ return (
     </p>
     <div className="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12">
       <a
-        href="https://slb.ssangyongsports.org/"
-        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
-      >
-        <span
-          style={{
-            boxSizing: "border-box",
-            display: "inline-block",
-            overflow: "hidden",
-            width: "initial",
-            height: "initial",
-            background: "none",
-            opacity: 1,
-            border: 0,
-            margin: 0,
-            padding: 0,
-            position: "relative",
-            maxWidth: "100%"
-          }}
-        >
-          <img
-            alt=""
-            className="service-icon"
-            src="https://74-rho.vercel.app/images/baseball-16.png"
-            width={75}
-          />
-          <noscript />
-        </span>
-        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          雙龍職棒
-        </h3>
-      </a>
-      <a
-        href="https://sba.ssangyongsports.org/"
-        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
-      >
-        <span
-          style={{
-            boxSizing: "border-box",
-            display: "inline-block",
-            overflow: "hidden",
-            width: "initial",
-            height: "initial",
-            background: "none",
-            opacity: 1,
-            border: 0,
-            margin: 0,
-            padding: 0,
-            position: "relative",
-            maxWidth: "100%"
-          }}
-        >
-          <img
-            alt=""
-            className="service-icon"
-            src="https://74-rho.vercel.app/images/889455.png"
-            width={75}
-          />
-          <noscript />
-        </span>
-        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          雙龍職籃
-        </h3>
-      </a>
-      <a
-        href="/"
+        href="https://flowbite-svelte.com"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -108,22 +44,105 @@ return (
         </h3>
       </a>
       <a
-        href="/tv"
+        href="https://flowbite-svelte.com"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
-        <img
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
             alt=""
             className="service-icon"
-            src="https://cdn-icons-png.flaticon.com/512/168/168843.png"
+            src="https://74-rho.vercel.app/images/53283.png"
             width={75}
           />
+          <noscript />
+        </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          雙龍體育TV
+          雙龍足球
+        </h3>
+      </a>
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://74-rho.vercel.app/images/53283.png"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          雙龍足球
+        </h3>
+      </a>
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://74-rho.vercel.app/images/53283.png"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          雙龍足球
         </h3>
       </a>
     </div>
   </div>
 </section>
+
 </>
 
   )
