a11f5e0c32e87df487672338d29780a34e412323
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-05 18:27:14 +0800

    Update sport.tsx

--- pages/components/sport.tsx
+++ pages/components/sport.tsx
@@ -4,44 +4,14 @@ return (
 <section className="bg-gray-50 dark:bg-gray-800 border-t border-b border-gray-100 dark:border-gray-700">
   <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16">
     <h2 className="mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 lg:text-center dark:text-white md:text-4xl">
-    我們的旗下運動
+      我們的旗下運動
     </h2>
     <p className="mb-10 text-lg font-normal text-gray-500 dark:text-gray-400 lg:text-center lg:text-xl lg:px-64 lg:mb-16">
-     我們擁有的體育活動
+      我們擁有的體育活動
     </p>
     <div className="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12">
       <a
-        href="https://www.flowbite-react.com"
-        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
-      >
-       <img
-  alt=""
-  className="service-icon"
-  src="https://74-rho.vercel.app/images/baseball-16.png"
-  width={75}
-/>
-
-        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          雙龍職籃
-        </h3>
-      </a>
-      <a
-        href="https://github.com/themesberg/flowbite-vue"
-        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
-      >
-        <img
-  alt=""
-  className="service-icon"
-  src="https://74-rho.vercel.app/images/889455.png"
-  width={75}
-/>
-
-        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-         雙龍職棒
-        </h3>
-      </a>
-      <a
-        href="https://flowbite-svelte.com"
+        href="https://slb.ssangyongsports.org/"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
         <span
@@ -60,44 +30,76 @@ return (
             maxWidth: "100%"
           }}
         >
-          <span
-            style={{
-              boxSizing: "border-box",
-              display: "block",
-              width: "initial",
-              height: "initial",
-              background: "none",
-              opacity: 1,
-              border: 0,
-              margin: 0,
-              padding: 0,
-              maxWidth: "100%"
-            }}
-          >
-            <img
-              alt=""
-              aria-hidden="true"
-              src="data:image/svg+xml,%3csvg%20xmlns=%27http://www.w3.org/2000/svg%27%20version=%271.1%27%20width=%2748%27%20height=%2748%27/%3e"
-              style={{
-                display: "block",
-                maxWidth: "100%",
-                width: "initial",
-                height: "initial",
-                background: "none",
-                opacity: 1,
-                border: 0,
-                margin: 0,
-                padding: 0
-              }}
-            />
-          </span>
-       <img
-  alt=""
-  className="service-icon"
-  src="https://74-rho.vercel.app/images/53283.png"
-  width={75}
-/>
-
+          <img
+            alt=""
+            className="service-icon"
+            src="https://74-rho.vercel.app/images/baseball-16.png"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          雙龍職棒
+        </h3>
+      </a>
+      <a
+        href="https://sba.ssangyongsports.org/"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://74-rho.vercel.app/images/889455.png"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          雙龍職籃
+        </h3>
+      </a>
+      <a
+        href="/"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://74-rho.vercel.app/images/53283.png"
+            width={75}
+          />
           <noscript />
         </span>
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
@@ -105,24 +107,23 @@ return (
         </h3>
       </a>
       <a
-        href="https://github.com/themesberg/flowbite-angular"
+        href="/tv"
         className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
       >
-        <svg
-          className="w-12 h-12 mx-auto text-red-600"
-          fill="currentColor"
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 448 512"
-        >
-          <path d="M185.7 268.1h76.2l-38.1-91.6-38.1 91.6zM223.8 32L16 106.4l31.8 275.7 176 97.9 176-97.9 31.8-275.7zM354 373.8h-48.6l-26.2-65.4H168.6l-26.2 65.4H93.7L223.8 81.5z" />
-        </svg>
+        <img
+            alt=""
+            className="service-icon"
+            src="https://cdn-icons-png.flaticon.com/512/168/168843.png"
+            width={75}
+          />
         <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
-          Flowbite Angular
+          雙龍體育TV
         </h3>
       </a>
     </div>
   </div>
 </section>
+
 
   )
 }
