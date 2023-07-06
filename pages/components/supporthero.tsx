755e5815596ab66510d38bef293d91787270e34b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 11:12:38 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -1,3 +1,4 @@
+import styles from './Button.module.css';
 
 
 function supporthero() {
@@ -8,7 +9,7 @@ function supporthero() {
 >
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      需要<span className="Button_abc__EPVsE">雙龍體育</span>的幫助嗎?
+      需要 <span className={styles.aabc}>雙龍體育</span>的幫助嗎?
     </h2>
   </div>
 </section>
