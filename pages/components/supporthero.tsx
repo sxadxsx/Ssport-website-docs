691ea488a37429a012907b17f7b8beec509a7573
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 22:20:55 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -5,7 +5,6 @@ function supporthero() {
   return (
 <section
   className="bg-white dark:bg-gray-900"
-  style={{ backgroundImage: "linear-gradient(to right, #86aed8, #c1b9db)" }}
 >
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
