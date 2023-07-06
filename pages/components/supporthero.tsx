f410810d4e8688f479eaa45b128280b014d4ab41
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 11:17:03 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -9,7 +9,7 @@ function supporthero() {
 >
   <div className="py-8 px-4 mx-auto max-w-screen-xl text-center lg:py-16 lg:px-12">
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
-      需要 <span className={styles.aabc}>雙龍體育</span>的幫助嗎?
+      需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
     </h2>
   </div>
 </section>
