2f1345c94819ce380e67eb55609eacc72a7984c3
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 13:02:09 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -8,7 +8,6 @@ import styles from './components/Button.module.css';
 // The Blog Page Content
 export default function Blog({posts}){
     return <main>
-        <br /> 
         <section className="bg-white dark:bg-gray-900">
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
