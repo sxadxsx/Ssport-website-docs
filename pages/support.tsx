3d4852f6f825638aa43299c5f5223380f016bb64
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 10:58:43 +0800

    Create support.tsx

---
+++ pages/support.tsx
@@ -0,0 +1,21 @@
+import Supportchoice from './components/supportchoice';
+import Supporthero from './components/supporthero';
+
+const support = () => {
+  return (
+       <>
+<Head>
+        <title>雙龍體育幫助</title>
+        <meta name="description" content="歡迎來到雙龍體育幫助中心,任何問題立刻聯繫" />
+      </Head>
+      
+      <div>
+               <Supporthero />
+
+        <Supportchoice />
+      </div>
+    </>
+      );
+}
+
+export default Home;
