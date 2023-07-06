d35f6205f2112658505f1c69aea80e0c9c4cbec9
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 12:58:04 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -9,7 +9,10 @@ import Head from 'next/head'
 export default function Post({frontmatter, content}) {
     const {title, seo, author, category, date, bannerImage, tags} = frontmatter
 
-    return <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
+    return 
+        <br /> 
+
+    <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
         <header className="mb-4 lg:mb-6 not-format">                   
