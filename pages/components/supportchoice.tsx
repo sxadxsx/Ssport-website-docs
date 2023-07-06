b139c4f2065328300d0e8880e1909aa0b2a0a334
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-06 10:54:33 +0800

    Create supportchoice.tsx

---
+++ pages/components/supportchoice.tsx
@@ -0,0 +1,144 @@
+
+function supportchoice() {
+  return (
+<section className="bg-gray-50 dark:bg-gray-800 border-t border-b border-gray-100 dark:border-gray-700">
+  <div className="max-w-screen-xl px-4 py-8 mx-auto lg:py-16">
+    <h2 className="mb-6 text-3xl font-extrabold leading-tight tracking-tight text-gray-900 lg:text-center dark:text-white md:text-4xl">
+      我們提供的支援方式
+    </h2>
+    <div className="space-y-4 sm:grid sm:grid-cols-2 lg:grid-cols-4 sm:gap-4 xl:gap-8 sm:space-y-0 md:mt-12">
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://th.bing.com/th/id/OIG.416Ubl3HuCXju7xRYO4m?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          幫助中心
+        </h3>
+      </a>
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://th.bing.com/th/id/OIG.C96gOk0chUn1kKHxKOg3?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          聯繫我們
+        </h3>
+      </a>
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://th.bing.com/th/id/OIG.cyGRgj1FvZpQO.xkVoOi?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          論壇
+        </h3>
+      </a>
+      <a
+        href="https://flowbite-svelte.com"
+        className="block px-8 py-12 text-center bg-white border border-gray-200 rounded-lg shadow-sm dark:bg-gray-700 dark:border-gray-600 hover:shadow-lg dark:hover:shadow-lg-light"
+      >
+        <span
+          style={{
+            boxSizing: "border-box",
+            display: "inline-block",
+            overflow: "hidden",
+            width: "initial",
+            height: "initial",
+            background: "none",
+            opacity: 1,
+            border: 0,
+            margin: 0,
+            padding: 0,
+            position: "relative",
+            maxWidth: "100%"
+          }}
+        >
+          <img
+            alt=""
+            className="service-icon"
+            src="https://th.bing.com/th/id/OIG.OaAnbr7kjUA3SffiMoCl?w=173&h=173&c=6&r=0&o=5&dpr=1.3&pid=ImgGn"
+            width={75}
+          />
+          <noscript />
+        </span>
+        <h3 className="font-semibold text-xl text-gray-900 dark:text-white mt-3.5">
+          狀態
+        </h3>
+      </a>
+    </div>
+  </div>
+</section>
+  )
+}
+
+export default supportchoice;
