83dcadc457dd36f3c5f4947a694464322c41488a
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-15 11:59:08 +0800

    Update ticket.tsx

--- pages/components/ticket.tsx
+++ pages/components/ticket.tsx
@@ -13,7 +13,7 @@ function ticket() {
 </h2>
       <div className="flex flex-col mb-8 md:flex-row lg:justify-center">
         <a
-          href="https://ticket.help.ssangyongsports.org/view.php"
+          href="https://support.ssangyongsports.org/view.php"
           className="text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center"
         >
           立刻查看
