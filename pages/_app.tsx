a6b9d6f2596dd98aeb7486bca601613c40dbaecd
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-21 21:11:51 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,10 +4,10 @@ import Head from './components/head'
 import Footer from './components/footer'
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    </div>
+    <div>
       
     <Head />
-    </div />
+      <div className="content">
 <Component {...pageProps} />
    </div>
         <Footer />
