07c109bcf32517629b48a9442ac75660fc0546d5
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-28 20:46:24 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -1,14 +1,13 @@
-import NextAuth from 'next-auth'
-import Providers from `next-auth/providers/Google`
-
-const options = {
+import NextAuth from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+export const authOptions = {
+  // Configure one or more authentication providers
   providers: [
     Providers.Google({
       clientId: process.env.GOOGLE_ID,
       clientSecret: process.env.GOOGLE_SECRET
-    }),
+   }),
+    // ...add more providers here
   ],
-  debug: false
 }
-
-export default (req, res) => NextAuth(req, res, options)
+export default NextAuth(authOptions)
