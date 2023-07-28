c3f07f6dd4580dd5e4e4ad50448d496f545e9b7e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-28 20:29:18 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,14 +2,15 @@ import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
+import { Provider } from 'next-auth/client'
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <div>
       
     <Head />
-      <div className="content">
-<Component {...pageProps} />
-   </div>
+       <Provider session={pageProps.session}>
+      <Component {...pageProps} />
+    </Provider>
         <Footer />
     
   </div>
