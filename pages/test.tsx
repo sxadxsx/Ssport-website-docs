8015fb350ef720637aa4780e2a65d886f78dbc74
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-28 21:06:16 +0800

    Update test.tsx

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,17 +1,39 @@
-import { signIn } from 'next-auth/react';
-import Link from 'next/link'
+import { useState, useEffect } from "react"
+import { useSession } from "next-auth/react"
+import AccessDenied from "../components/accessDenied.js"
 
-export default function AccessDenied () {
+export default function ProtectedPage() {
+  const { data: session } = useSession()
+  const [content, setContent] = useState()
+
+  // Fetch content from protected route
+  useEffect(() => {
+    const fetchData = async () => {
+      const res = await fetch("/api/examples/protected")
+      const json = await res.json()
+      if (json.content) {
+        setContent(json.content)
+      }
+    }
+    fetchData()
+  }, [session])
+
+  // If no session exists, display access denied message
+  if (!session) {
+    return (
+      <div>
+        <AccessDenied />
+      </div>
+    )
+  }
+
+  // If session exists, display content
   return (
-    <>
-      <h1>Access Denied</h1>
+    <div>
+      <h1>Protected Page</h1>
       <p>
-        <Link href="/api/auth/signin"
-           onClick={(e) => {
-           e.preventDefault()
-           signIn()
-        }}>You must be signed in to view this page</Link>
+        <strong>{content ?? "\u00a0"}</strong>
       </p>
-    </>
+    </div>
   )
 }
