e93ebc20199cb7f8e01b6dda36c15fd07990446b
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-28 20:32:16 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,11 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
-import Head from './components/head'
-import Footer from './components/footer'
-import { Provider } from 'next-auth/client'
-export default function App({ Component, pageProps }: AppProps) {
+import { SessionProvider } from "next-auth/react"
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}) {
   return (
-    <div>
-      
-    <Head />
-       <Provider session={pageProps.session}>
+    <SessionProvider session={session}>
       <Component {...pageProps} />
-    </Provider>
-        <Footer />
-    
-  </div>
-
-  );
+    </SessionProvider>
+  )
 }
