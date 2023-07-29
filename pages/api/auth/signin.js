f2dd797ef6217a611a81c317a000fe2407d7a039
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-30 01:02:18 +0800

    Update signin.js

--- pages/api/auth/signin.js
+++ pages/api/auth/signin.js
@@ -1,56 +1,37 @@
-import React, { useState, useEffect } from 'react'
-import { getCsrfToken, getProviders, useSession } from "next-auth/react"
-import { useRouter } from "next/router";
-import { Alert, Col, Container, Row } from 'react-bootstrap';
-import { SignInForm } from './SignInForm';
 
-export default function signin({...props}) {
-  const [providers, setProviders] = useState(null);
-  const [csrfToken, setCsrfToken] = useState(null);
-  const router = useRouter()
-  const { data: session, status } = useSession()
 
-  if (status === 'authenticated') {
-    router.push('/');
-  }
+import type { GetServerSidePropsContext, InferGetServerSidePropsType } from "next";
+import { getProviders, signIn } from "next-auth/react"
+import { getServerSession } from "next-auth/next"
+import { authOptions } from "../api/auth/[...nextauth]";
 
-  useEffect(async() => {
-    if (status !== 'loading') {
-      if (await providers === null) {
-        setProviders(await getProviders());
-        if (await csrfToken === null) {
-          setCsrfToken(await getCsrfToken());
-        }
-      }
-    }
-    
-    return () => {
-      // cleanup
-    }
-  });
-
-  const { query } = useRouter();
-
+export default function SignIn({ providers }: InferGetServerSidePropsType<typeof getServerSideProps>) {
   return (
-    <>                
-      <div className="d-flex align-items-center min-vh-100 bg-auth border-top border-top-2 border-primary">
-        <Container>
-          <Row className="justify-content-center">
-            <Col xs={12} md={5} xl={4} className="my-5">
-              {query.error && (
-                <>
-                  <Alert  variant="danger">
-                      Could not login. Please check your e-mail or password or third-party application.
-                    </Alert>
-                </>
-              )}
-
-              <SignInForm providers={providers} csrfToken={csrfToken} query={query} signup={props.signup ? true : false} />
-              
-            </Col>
-          </Row>
-        </Container>
-      </div>
+    <>
+      {Object.values(providers).map((provider) => (
+        <div key={provider.name}>
+          <button onClick={() => signIn(provider.id)}>
+            Sign in with {provider.name}
+          </button>
+        </div>
+      ))}
     </>
   )
+}
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const session = await getServerSession(context.req, context.res, authOptions);
+  
+  // If the user is already logged in, redirect.
+  // Note: Make sure not to redirect to the same page
+  // To avoid an infinite loop!
+  if (session) {
+    return { redirect: { destination: "/" } };
+  }
+
+  const providers = await getProviders();
+  
+  return {
+    props: { providers: providers ?? [] },
+  }
 }
