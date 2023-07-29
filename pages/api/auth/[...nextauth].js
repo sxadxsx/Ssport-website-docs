7b5294297aa65229af0fdb3985e9e9f2b3ace88c
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 22:01:32 +0800

    Update [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,6 +12,10 @@ export const authOptions = {
       clientId: process.env.Google_ID,
       clientSecret: process.env.Google_SECRET,
     }),  ],
+  theme: {
+  colorScheme: "light", // "auto" | "dark" | "light"
+  logo: "https://i.imgur.com/GhQm5zm.png", // Absolute URL to image
+    },
 callbacks: {
   async jwt({ token, account }) {
     // Persist the OAuth access_token to the token right after signin
@@ -25,10 +29,7 @@ callbacks: {
     session.accessToken = token.accessToken
     return session
     },
-    theme: {
-  colorScheme: "light", // "auto" | "dark" | "light"
-  logo: "https://i.imgur.com/GhQm5zm.png", // Absolute URL to image
-    },
+    
   },
 }
 
