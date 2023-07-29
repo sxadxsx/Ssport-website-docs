bd0f1ae538ac21ff5763ee6c56fdd7f067176978
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 20:43:33 +0800

    更新 [...nextauth].js

--- pages/api/auth/[...nextauth].js
+++ pages/api/auth/[...nextauth].js
@@ -12,47 +12,22 @@ export const authOptions = {
       clientId: process.env.Google_ID,
       clientSecret: process.env.Google_SECRET,
     }),  ],
-return await NextAuth(req, res, {
-providers,
-secret: process.env.SECRET,
-jwt: {
-secret: process.env.SECRET,
-},
-session: {
-// This is the default. The session is saved in a cookie and never persisted anywhere.
-strategy: "jwt",
-},
-// Enable debug messages in the console if you are having problems
-debug: true,
-
-pages: {
-signIn: "/auth/signin",
-error: "/auth/signin",
-newUser: "/auth/new-user",
-},
-
 callbacks: {
-async session({ session, token }) {
-// Send properties to the client, like an access_token from a provider.
-session.accessToken = token.accessToken;
-session.refreshToken = token.refreshToken;
-session.idToken = token.idToken;
-session.provider = token.provider;
-session.id = token.id;
-return session;
-},
-async jwt({ token, user, account }) {
-// Persist the OAuth access_token to the token right after signin
-if (account) {
-token.accessToken = account.access_token;
-token.refreshToken = account.refresh_token;
-token.idToken = account.id_token;
-token.provider = account.provider;
-}
-if (user) {
-token.id = user.id.toString();
-}
-return token;
+  async jwt({ token, account }) {
+    // Persist the OAuth access_token to the token right after signin
+    if (account) {
+      token.accessToken = account.access_token
+    }
+    return token
+  },
+  async session({ session, token, user }) {
+    // Send properties to the client, like an access_token from a provider.
+    session.accessToken = token.accessToken
+    return session
+    },
+    theme: {
+  colorScheme: "light", // "auto" | "dark" | "light"
+  logo: "https://i.imgur.com/GhQm5zm.png", // Absolute URL to image
     },
   },
 }
