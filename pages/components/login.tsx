1d7df7a05018927d434ef85795fd960d09d622f5
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 20:15:11 +0800

    更新 login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,7 +1,7 @@
 import { useSession, signIn, signOut } from 'next-auth/react';
 import React from 'react';
 import { Button } from "flowbite-react";
-export const Login = () => {
+export default = () => {
 const { data: session, status } = useSession();
 if (session) {
 return (
