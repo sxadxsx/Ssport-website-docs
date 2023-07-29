dac0c98d6ed757c744ca8035448144a7172e12f6
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 23:16:17 +0800

    Update login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -3,27 +3,28 @@ import { signIn, signOut, useSession } from "next-auth/react"
 import React from 'react';
 import { Button } from "flowbite-react";
 import { Dropdown, Navbar, Avatar } from 'flowbite-react';
+import Link from 'next/link'
 export default function Login() {
 const { data: session, status } = useSession();
 if (session) {
 return (
 <>
-<Dropdown inline label={<Avatar alt="User settings" img="https://th.bing.com/th/id/OIG.3ATeLzhfld0KGXFir2Vh?w=173&h=173&c=6&r=0&o=5&dpr=2.8&pid=ImgGn" rounded/>} > <Dropdown.Header> <span className="block text-sm"> {session.user.name}  </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <div> <a 
-                 href={`/api/auth/signout`} 
+<Dropdown inline label={<Avatar alt="User settings" img="https://th.bing.com/th/id/OIG.3ATeLzhfld0KGXFir2Vh?w=173&h=173&c=6&r=0&o=5&dpr=2.8&pid=ImgGn" rounded/>} > <Dropdown.Header> <span className="block text-sm"> {session.user.name}  </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <div>
+  <Link href={`/api/auth/signout`} 
                  onClick={(e) => { 
                    e.preventDefault() 
                    signOut() 
                  }} 
                > 
                  登出
-               </a> </div> </Dropdown>
+               </Link> </div> </Dropdown>
 </>
 )
 }
 return (
 <>
 <Button> 
-  <a 
+  <Link
                  href={`/api/auth/signin`} 
                  onClick={(e) => { 
                    e.preventDefault() 
@@ -31,7 +32,7 @@ return (
                  }} 
                > 
                  登入
-               </a></Button>
+               </Link></Button>
 </>
 )
 }
