9f68427cc4c7e18aa76322de573586c46334d711
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 21:47:08 +0800

    更新 login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -8,7 +8,7 @@ const { data: session, status } = useSession();
 if (session) {
 return (
 <>
-<Dropdown inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>} > <Dropdown.Header> <span className="block text-sm"> Bonnie Green </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <div> <a 
+<Dropdown inline label={<Avatar alt="User settings" img="{session.user.image}" rounded/>} > <Dropdown.Header> <span className="block text-sm"> {session.user.name}  </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <div> <a 
                  href={`/api/auth/signout`} 
                  onClick={(e) => { 
                    e.preventDefault() 
