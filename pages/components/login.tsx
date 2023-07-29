3d19b756bd4a0a24e0a8601db75bb689574242c4
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 21:43:32 +0800

    更新 login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -8,7 +8,7 @@ const { data: session, status } = useSession();
 if (session) {
 return (
 <>
-<Dropdown inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>} > <Dropdown.Header> <span className="block text-sm"> Bonnie Green </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <Item> <a 
+<Dropdown inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>} > <Dropdown.Header> <span className="block text-sm"> Bonnie Green </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <div> Dashboard </div> <div> Settings </div> <div> Earnings </div> <Dropdown.Divider /> <div> <a 
                  href={`/api/auth/signout`} 
                  onClick={(e) => { 
                    e.preventDefault() 
@@ -16,7 +16,7 @@ return (
                  }} 
                > 
                  登出
-               </a> </Item> </Dropdown>
+               </a> </div> </Dropdown>
 </>
 )
 }
