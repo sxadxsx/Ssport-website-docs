5b8f89ae0c3b23e6fb3b59ce102b39feab141589
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-07-29 21:36:40 +0800

    更新 login.tsx

--- pages/components/login.tsx
+++ pages/components/login.tsx
@@ -1,13 +1,13 @@
 import { signIn, signOut, useSession } from "next-auth/react"
 import React from 'react';
 import { Button } from "flowbite-react";
+import { Dropdown, Navbar } from 'flowbite-react';
 export default function Login() {
 const { data: session, status } = useSession();
 if (session) {
 return (
 <>
-
-<Button>         <a 
+<Dropdown inline label={<Avatar alt="User settings" img="https://flowbite.com/docs/images/people/profile-picture-5.jpg" rounded/>} > <Dropdown.Header> <span className="block text-sm"> Bonnie Green </span> <span className="block truncate text-sm font-medium"> {session.user.email} </span> </Dropdown.Header> <Item> Dashboard </Item> <Item> Settings </Item> <Item> Earnings </Item> <Dropdown.Divider /> <Item> <a 
                  href={`/api/auth/signout`} 
                  onClick={(e) => { 
                    e.preventDefault() 
@@ -15,8 +15,7 @@ return (
                  }} 
                > 
                  登出
-               </a>
-</Button>
+               </a> </Item> </Dropdown>
 </>
 )
 }
