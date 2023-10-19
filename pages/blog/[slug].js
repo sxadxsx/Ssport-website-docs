920bbaab6a953f9d7bee1a5046c701800dc843ca
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-10-20 00:05:11 +0800

    Update [slug].js

--- pages/blog/[slug].js
+++ pages/blog/[slug].js
@@ -13,13 +13,7 @@ export default function Post({frontmatter, content}) {
     return <main className="pt-8 pb-16 lg:pt-16 lg:pb-24 bg-white dark:bg-gray-900">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
-        <div>
-  <img
-    className="h-auto max-w-full rounded-lg"
-    src={bannerImage}
-    alt=""
-  />
-</div>
+        
 
         <header className="mb-4 lg:mb-6 not-format">                   
           <h1 className={`mb-4 text-3xl font-extrabold leading-tight text-gray-900 lg:mb-6 lg:text-4xl dark:text-white ${styles.abc}`}>
