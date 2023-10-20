cf742031b76241c66d4565ced94876bc7e2c26b1
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-10-20 21:02:08 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,5 +1,3 @@
-import { useEffect } from 'react'
-import { useRouter } from 'next/router'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
@@ -7,33 +5,22 @@ import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
 
 export default function App({
-  Component,
+  Component, 
   pageProps: { session, ...pageProps },
 }: AppProps) {
-  const router = useRouter()
-
-  useEffect(() => {
-    const handleRouteChange = () => {
-      // 在每次路由變更時重新載入 Groove 小工具
-      if (typeof window !== 'undefined' && window.groove) {
-        window.groove.widget.init('c204c904-e504-4986-b1c3-e7d5e890a62d', {})
-      }
-    }
-
-    router.events.on('routeChangeComplete', handleRouteChange)
-
-    return () => {
-      router.events.off('routeChangeComplete', handleRouteChange)
-    }
-  }, [])
 
   return (
     <SessionProvider session={session}>
-      <div>
+        <div>
+
         <Head />
-        <Component {...pageProps} />
-        <Footer />
-      </div>
+    
+      <Component {...pageProps} />
+    
+            <Footer />
+        </div>
     </SessionProvider>
+
   )
+
 }
