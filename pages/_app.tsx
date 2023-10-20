764d2e40d297f4b646e6a0e3bfcbfcca4e8f7cb1
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-10-20 17:54:10 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,35 +1,39 @@
-import { useEffect } from 'react';
+import { useEffect } from 'react'
+import { useRouter } from 'next/router'
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
-const App = ({ Component, pageProps }) => {
-  useEffect(() => {
-    if (typeof window !== 'undefined' && !window.groove) {
-      const script = document.createElement('script');
-      script.id = 'groove-script';
-      script.src = 'https://c204c904-e504-4986-b1c3-e7d5e890a62d.widget.cluster.groovehq.com/api/loader';
-      document.body.appendChild(script);
-    }
-  }, []);
 
-  
+export default function App({
+  Component,
+  pageProps: { session, ...pageProps },
+}: AppProps) {
+  const router = useRouter()
+
+  useEffect(() => {
+    const handleRouteChange = () => {
+      // 在每次路由變更時重新載入 Groove 小工具
+      if (typeof window !== 'undefined' && window.groove) {
+        window.groove.widget.init('c204c904-e504-4986-b1c3-e7d5e890a62d', {})
+      }
+    }
+
+    router.events.on('routeChangeComplete', handleRouteChange)
+
+    return () => {
+      router.events.off('routeChangeComplete', handleRouteChange)
+    }
+  }, [])
 
   return (
     <SessionProvider session={session}>
-        <div>
+      <div>
         <Head />
-    
-      <Component {...pageProps} />
-    
-            <Footer />
-        </div>
-       <div>
-      
-          </div>
+        <Component {...pageProps} />
+        <Footer />
+      </div>
     </SessionProvider>
-
   )
-
 }
