d55e2df4cd3db47cceaec037e348f6b40e43554e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-10-28 18:06:19 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -4,23 +4,22 @@ import Head from './components/head'
 import Footer from './components/footer'
 import { SessionProvider } from 'next-auth/react'
 import Script from 'next/script';
+
 export default function App({
   Component, 
   pageProps: { session, ...pageProps },
 }: AppProps) {
-
   return (
-        
-    <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
-        <Script src="/chat.js" />
-         <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
-<SessionProvider session={session}>
-        <Head />
-        <Component {...pageProps} />
-        <Footer />
-            </SessionProvider>
-           </main>
-      </div>
-   
+    <div>
+      <div className="fixed h-screen w-full bg-gradient-to-br from-indigo-50 via-white to-cyan-100" />
+      <main className="flex min-h-screen w-full flex-col items-center justify-center py-32">
+        <SessionProvider session={session}>
+          <Head />
+          <Component {...pageProps} />
+          <Footer />
+        </SessionProvider>
+      </main>
+      <Script src="/chat.js" />
+    </div>
   );
 }
