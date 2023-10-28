0b18e5255d584ff98a600a5032d612502a8c1869
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-28 19:05:08 +0800

    Update tabl.tsx

--- pages/components/tabl.tsx
+++ pages/components/tabl.tsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 
 function Tabl() {
   return (
-    <section className="bg-white dark:bg-gray-900">
+    <section>
       <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
         <div className="mt-4 md:mt-0">
           <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white `}>
