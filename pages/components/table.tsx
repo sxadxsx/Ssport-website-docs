90261f9013726b8e4989e032508161d520e100d6
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-28 19:04:09 +0800

    Update table.tsx

--- pages/components/table.tsx
+++ pages/components/table.tsx
@@ -4,7 +4,7 @@ import styles from './Button.module.css';
 
 function table() {
   return (
-<section className="bg-white dark:bg-gray-900">
+<section>
   <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
     <Image
       className="w-full dark:hidden"
