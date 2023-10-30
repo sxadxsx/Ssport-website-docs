2af1372c0fe9bff550f15fc16eb6aec11b1cadec
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-30 21:11:13 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,7 +11,23 @@ export default function App({
 }: AppProps) {
   return (
     <div>
-      
+      <script type="module">
+  import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.10/dist/web.js'
+
+  Typebot.initBubble({
+    typebot: "customer-support-q6zv3pv",
+    apiHost: "https://ssportbotview.vercel.app",
+    previewMessage: {
+      message: "I have a question for you!",
+      avatarUrl:
+        "https://ssangyongsports.eu.org/_next/image?url=%2Fteam.jpeg&w=64&q=75",
+    },
+    theme: {
+      button: { backgroundColor: "#0042DA" },
+      chatWindow: { backgroundColor: "#ffffff" },
+    },
+  });
+</script>
       
         <SessionProvider session={session}>
           <Head />
