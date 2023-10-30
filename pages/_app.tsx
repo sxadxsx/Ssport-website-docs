a032d532c64becc2fe4efb512fd7124e8a1f1f5a
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-10-30 21:59:09 +0800

    Update _app.tsx

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -11,22 +11,7 @@ export default function App({
 }: AppProps) {
   return (
     <div>
-      <script type="module">
-  const typebotInitScript = document.createElement("script");
-typebotInitScript.type = "module";
-typebotInitScript.innerHTML = `import Typebot from 'https://cdn.jsdelivr.net/npm/@typebot.io/js@0.2.10/dist/web.js'
-
-Typebot.initBubble({
-  typebot: "lead-generation-ils4v1g",
-  apiHost: "https://ssportbotview.vercel.app",
-  theme: {
-    button: { backgroundColor: "#0042DA", iconColor: "#FFFFFF" },
-    previewMessage: { backgroundColor: "#ffffff", textColor: "black" },
-  },
-});
-`;
-document.body.append(typebotInitScript);
-</script>
+      
       
         <SessionProvider session={session}>
           <Head />
