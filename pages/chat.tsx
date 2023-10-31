8ef984c0080c813c2a4ecb8d436357083bcc64a3
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-01 00:18:02 +0800

    Update chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -5,8 +5,9 @@ function chat() {
         <div>
 <iframe
   src="https://ssportbotview.vercel.app/customer-support-q6zv3pv"
-  style="border: none; width=100%; height=100vh"
-></iframe>
+  style={{ border: "none" }}
+/>
+
 </div>
   )
 }
