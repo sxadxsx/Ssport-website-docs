623eaed02ebe7bcf1c4b86109b6e6f248b17bddf
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-01 00:13:18 +0800

    Create chat.tsx

---
+++ pages/chat.tsx
@@ -0,0 +1,14 @@
+import Head from 'next/head'
+
+function chat() {
+  return (   
+        <div>
+<iframe
+  src="https://ssportbotview.vercel.app/customer-support-q6zv3pv"
+  style="border: none; width=100%; height=100vh"
+></iframe>
+</div>
+  )
+}
+
+export default chat
