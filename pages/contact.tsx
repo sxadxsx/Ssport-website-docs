94fa4d450610c980ed49e8a7e4637f9cd1e40056
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-03 17:35:44 +0800

    Update contact.tsx

--- pages/contact.tsx
+++ pages/contact.tsx
@@ -6,7 +6,7 @@ import { FileInput } from 'flowbite-react';
 function c() {
   return (   
         <div>
-   <section className="bg-white dark:bg-gray-900">
+   <section>
   <div className="py-8 lg:py-16 px-4 mx-auto max-w-screen-md">
     <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-center text-gray-900 dark:text-white">
       <span className={styles.abc}>聯繫</span>
