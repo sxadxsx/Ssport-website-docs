e281df68d4d2f4a8851d16864f2f9163232b4a93
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-03 17:39:24 +0800

    Update p.tsx

--- pages/p.tsx
+++ pages/p.tsx
@@ -2,7 +2,7 @@ import Head from 'next/head'
 const p = () => {
   return (
 
-<main>
+<main className="pt-8 pb-16 lg:pt-16 lg:pb-24">
   <div className="flex justify-between px-4 mx-auto max-w-screen-xl ">
     <article className="mx-auto w-full max-w-2xl format format-sm sm:format-base lg:format-lg format-blue dark:format-invert">
         <header className="mb-4 lg:mb-6 not-format">        
