c3ceb391c6a65ad5dd654a87b3c5bf006a357abf
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-06 23:48:05 +0800

    Update ticket.tsx

--- pages/components/ticket.tsx
+++ pages/components/ticket.tsx
@@ -1,28 +1,53 @@
  import Link from 'next/link';
+import Image from 'next/image';
 import styles from './Button.module.css';
 
-function ticket() {
+function Tabl() {
   return (
-<section className="bg-white dark:bg-gray-900">
-  <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-    <div className="mx-auto max-w-screen-md sm:text-center">
-      <h2 className="mb-4 text-3xl tracking-tight font-extrabold text-gray-900 sm:text-4xl dark:text-white">
-也可嘗試我們的
-          <span className={styles.abc}>機器人🤖️</span>
-協助(系統測試中)
-</h2>
-      <div className="flex flex-col mb-8 md:flex-row lg:justify-center">
-        <Link
-          href="/chat"
-          className="text-white bg-blue-700 hover:bg-blue-800 dark:bg-blue-600 focus:ring-4 focus:ring-blue-300 dark:focus:ring-blue-800 font-medium rounded-lg text-base px-6 py-2.5 text-center md:mr-5 mb-3 md:mb-0 inline-flex items-center justify-center"
-        >
-          立刻與機器人聊天
-        </Link>
+    <section>
+      <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
+        <div className="mt-4 md:mt-0">
+          <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white `}>
+            與
+                        <span className={styles.abc}>機器人</span>
+           進行交談
+          </h2>
+         
+        </div>
+        <iframe
+            
+            srcDoc="<body><script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
+            <script>
+              window.botpressWebChat.init({
+                  'composerPlaceholder': 'Chat with bot',
+                  'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
+                  'botName': 'Name',
+                  'botId': 'f87c4530-e481-49dd-8841-236d83aa1389',
+                  'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
+                  'messagingUrl': 'https://messaging.botpress.cloud',
+                  'clientId': 'f87c4530-e481-49dd-8841-236d83aa1389',
+                  'enableConversationDeletion': true,
+                  'showPoweredBy': true,
+                  'className': 'webchatIframe',
+                  'containerWidth': '100%25',
+                  'layoutWidth': '100%25',
+                  'hideWidget': true,
+                  'showCloseButton': false,
+                  'disableAnimations': true,
+                  'closeOnEscape': false,
+                  'showConversationsButton': false,
+                  'enableTranscriptDownload': false,
+                  'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/3fcd3e4e-d5bc-4bf5-8699-14b621b3ada2/v31782/style.css'
+                  
+              });
+            window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
+            </script></body>"
+            width="100%"
+            height="500%"
+          ></iframe>
       </div>
-    </div>
-  </div>
-</section>
-  )
+    </section>
+  );
 }
 
-export default ticket;
+export default Ticket;
