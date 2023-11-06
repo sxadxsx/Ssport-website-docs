8eb40e106d92844fbaab4fba6647ee5d199de5d8
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-06 23:42:34 +0800

    Update chat.tsx

--- pages/chat.tsx
+++ pages/chat.tsx
@@ -12,11 +12,37 @@ function chat() {
     <p className="mb-8 lg:mb-16 font-light text-center text-gray-500 dark:text-gray-400 sm:text-xl">
       有任何問題，立刻詢問機器人
     </p>
-          <iframe
-  src="https://ssportbotview.vercel.app/customer-support-q6zv3pv"
-  width="100%"
-  height={400}
-></iframe>
+            <iframe
+            style="border: none;"
+            srcdoc="<body><script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
+            <script>
+              window.botpressWebChat.init({
+                  'composerPlaceholder': 'Chat with bot',
+                  'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
+                  'botName': 'Name',
+                  'botId': 'f87c4530-e481-49dd-8841-236d83aa1389',
+                  'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
+                  'messagingUrl': 'https://messaging.botpress.cloud',
+                  'clientId': 'f87c4530-e481-49dd-8841-236d83aa1389',
+                  'enableConversationDeletion': true,
+                  'showPoweredBy': true,
+                  'className': 'webchatIframe',
+                  'containerWidth': '100%25',
+                  'layoutWidth': '100%25',
+                  'hideWidget': true,
+                  'showCloseButton': false,
+                  'disableAnimations': true,
+                  'closeOnEscape': false,
+                  'showConversationsButton': false,
+                  'enableTranscriptDownload': false,
+                  'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/3fcd3e4e-d5bc-4bf5-8699-14b621b3ada2/v31782/style.css'
+                  
+              });
+            window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
+            </script></body>"
+            width="100%"
+            height="100%"
+          ></iframe>
     </div>
 </section>
 </div>
