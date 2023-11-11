18eb8dd4e07cb268151c745c349c2588f9854c1e
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-11 10:54:23 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -12,7 +12,7 @@ export default function Blog({posts}){
         <section>
   <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
     <div className="mx-auto max-w-screen-sm text-center lg:mb-16 mb-8">
-      <h2 className={`text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500 `}>
+      <h2 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
         雙龍體育
             部落格
       </h2>
