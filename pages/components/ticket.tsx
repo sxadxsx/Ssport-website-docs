39b120c62e0383d9b31f702fb4f09527bf47126a
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-11 19:22:03 +0800

    Update ticket.tsx

--- pages/components/ticket.tsx
+++ pages/components/ticket.tsx
@@ -4,51 +4,15 @@ import css from './Button.module.css';
 import styles from './chat.module.css';
 function Ticket() {
   return (
-    <section>
-      <div className="gap-8 items-center py-8 px-4 mx-auto max-w-screen-xl xl:gap-16 md:grid md:grid-cols-2 sm:py-16 lg:px-6">
-        <div className="mt-4 md:mt-0">
-          <h2 className={`mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white `}>
-            與
-                        <span className={css.abc}>機器人</span>
-           進行交談
-          </h2>
-         
-        </div>
-       <div className={`${styles.centerDiv} ${styles.relative} ${styles.hFull} ${styles.wFull} ${styles.overflowClip} ${styles.roundedMd} ${styles.border} ${styles.borderZinc200} ${styles.bgWhite} ${styles.p2} ${styles.px0} ${styles.py0}`}>
-        <iframe
-            
-            srcDoc="<body><script src='https://cdn.botpress.cloud/webchat/v0/inject.js'></script>
-            <script>
-              window.botpressWebChat.init({
-                  'composerPlaceholder': 'Chat with bot',
-                  'botConversationDescription': 'This chatbot was built surprisingly fast with Botpress',
-                  'botName': 'Name',
-                  'botId': 'f87c4530-e481-49dd-8841-236d83aa1389',
-                  'hostUrl': 'https://cdn.botpress.cloud/webchat/v0',
-                  'messagingUrl': 'https://messaging.botpress.cloud',
-                  'clientId': 'f87c4530-e481-49dd-8841-236d83aa1389',
-                  'enableConversationDeletion': true,
-                  'showPoweredBy': true,
-                  'className': 'webchatIframe',
-                  'containerWidth': '100%25',
-                  'layoutWidth': '100%25',
-                  'hideWidget': true,
-                  'showCloseButton': false,
-                  'disableAnimations': true,
-                  'closeOnEscape': false,
-                  'showConversationsButton': false,
-                  'enableTranscriptDownload': false,
-                  'stylesheet':'https://webchat-styler-css.botpress.app/prod/code/3fcd3e4e-d5bc-4bf5-8699-14b621b3ada2/v31782/style.css'
-                  
-              });
-            window.botpressWebChat.onEvent(function () { window.botpressWebChat.sendEvent({ type: 'show' }) }, ['LIFECYCLE.LOADED']);
-            </script></body>"
-            width="100%"
-            height="400"
-          ></iframe>
-       </div>
-      </div>
-    </section>
+    <!--
+// v0 by Vercel.
+// https://v0.dev/t/0W13RkH
+-->
+<section class="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-black"><div class="container px-4 md:px-6"><div class="grid gap-6 items-center"><div class="flex flex-col justify-center space-y-8 text-center"><div class="space-y-2"><h1 class="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+            Discover Our Unique Features
+          </h1><p class="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
+            Our features are designed to enhance your productivity and streamline your workflow.
+          </p></div><div class="w-full max-w-full space-y-4 mx-auto"><div class="grid grid-cols-3 gap-8"><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><polyline points="22 12 16 12 14 15 10 15 8 12 2 12"></polyline><path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z"></path></svg></div><h2 class="text-xl font-bold text-white">Smart Inbox</h2><p class="text-zinc-200 dark:text-zinc-100">Our Smart Inbox feature helps you manage your emails efficiently by prioritizing important emails.</p></div><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><path d="m8 6 4-4 4 4"></path><path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22"></path><path d="m20 22-5-5"></path></svg></div><h2 class="text-xl font-bold text-white">Seamless Integration</h2><p class="text-zinc-200 dark:text-zinc-100">Seamless Integration allows you to connect with your favorite apps and services without leaving your inbox.</p></div><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z"></path><circle cx="12" cy="12" r="3"></circle></svg></div><h2 class="text-xl font-bold text-white">Advanced Customization</h2><p class="text-zinc-200 dark:text-zinc-100">With Advanced Customization, you can personalize your email client to suit your preferences and work style.</p></div><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><circle cx="11" cy="11" r="8"></circle><path d="m21 21-4.3-4.3"></path></svg></div><h2 class="text-xl font-bold text-white">Powerful Search</h2><p class="text-zinc-200 dark:text-zinc-100">Our Powerful Search feature allows you to find any email, contact, or file in seconds.</p></div><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><rect width="18" height="11" x="3" y="11" rx="2" ry="2"></rect><path d="M7 11V7a5 5 0 0 1 10 0v4"></path></svg></div><h2 class="text-xl font-bold text-white">Reliable Security</h2><p class="text-zinc-200 dark:text-zinc-100">With Reliable Security, your data is always safe and protected.</p></div><div class="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg"><div class="p-2 bg-black bg-opacity-50 rounded-full"><svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class=" text-white h-6 w-6 mb-2 opacity-75"><path d="m8 6 4-4 4 4"></path><path d="M12 2v10.3a4 4 0 0 1-1.172 2.872L4 22"></path><path d="m20 22-5-5"></path></svg></div><h2 class="text-xl font-bold text-white">Easy Collaboration</h2><p class="text-zinc-200 dark:text-zinc-100">Easy Collaboration allows you to share and edit documents with your team in real time.</p></div></div></div></div></div></div></section>
   );
 }
 
