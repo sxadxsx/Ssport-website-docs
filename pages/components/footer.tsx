7607d7c448581f0c403ca0919241979ca7d53de9
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-11 13:33:22 +0800

    Update footer.tsx

--- pages/components/footer.tsx
+++ pages/components/footer.tsx
@@ -22,7 +22,7 @@ function footer() {
            </li>
           <li className="mb-4">
                <a id="footer-badge" href="https://status.ssangyongsports.eu.org">
-        狀態載入中
+        載入狀態
       </a>
 
        <script
@@ -30,7 +30,7 @@ function footer() {
           __html: `
             function updateFooterBadge(status) {
               const badge = document.getElementById('footer-badge');
-              badge.textContent = status === 'UP' ? '系統運行' : '狀態異常';
+              badge.textContent = status === 'UP' ? '系統運行中' : '狀態異常';
               badge.className = status.toLowerCase();
             }
 
