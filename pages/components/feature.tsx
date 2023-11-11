fcc9c1804458140c7dc499e180e3053e5042e756
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-11 19:57:48 +0800

    Update feature.tsx

--- pages/components/feature.tsx
+++ pages/components/feature.tsx
@@ -1,21 +1,24 @@
 import styles from './Button.module.css';
 function feature() {
 return (
-<section className="bg-white dark:bg-gray-900">
-  <div className="py-8 px-4 mx-auto max-w-screen-xl sm:py-16 lg:px-6">
-    <div className="max-w-screen-md mb-8 lg:mb-16">
-      <h2 className="mb-4 text-4xl tracking-tight font-extrabold text-gray-900 dark:text-white">
-        優點及
-      <span className={styles.abc}>好處</span>
-      </h2>
-      <p className="text-gray-500 sm:text-xl dark:text-gray-400">
-       提供了使用的優點
-      </p>
-    </div>
-    <div className="space-y-8 md:grid md:grid-cols-2 lg:grid-cols-3 md:gap-12 md:space-y-0">
-      <div>
-        <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
-          <svg
+
+    <section className="w-full py-12 md:py-24 lg:py-32 xl:py-48 bg-black">
+      <div className="container px-4 md:px-6">
+        <div className="grid gap-6 items-center">
+          <div className="flex flex-col justify-center space-y-8 text-center">
+            <div className="space-y-2">
+              <h1 className="text-3xl font-bold tracking-tighter sm:text-5xl xl:text-6xl/none bg-clip-text text-transparent bg-gradient-to-r from-white to-gray-500">
+           發現我們的獨特之處
+              </h1>
+              <p className="max-w-[600px] text-zinc-200 md:text-xl dark:text-zinc-100 mx-auto">
+                我們的優點
+              </p>
+            </div>
+            <div className="w-full max-w-full space-y-4 mx-auto">
+              <div className="grid grid-cols-3 gap-8">
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 bg-black bg-opacity-50 rounded-full">
+                    <svg
             className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
             fill="currentColor"
             viewBox="0 0 20 20"
@@ -27,14 +30,18 @@ return (
               clipRule="evenodd"
             />
           </svg>
-        </div>
-        <h3 className="mb-2 text-xl font-bold dark:text-white">易設定</h3>
-        <p className="text-gray-500 dark:text-gray-400">
-      簡單快速上手
-        </p>
-      </div>
-      <div>
-        <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                      <polyline points="22 12 16 12 14 15 10 15 8 12 2 12" />
+                      <path d="M5.45 5.11 2 12v6a2 2 0 0 0 2 2h16a2 2 0 0 0 2-2v-6l-3.45-6.89A2 2 0 0 0 16.76 4H7.24a2 2 0 0 0-1.79 1.11z" />
+                    </svg>
+                  </div>
+                  <h2 className="text-xl font-bold text-white">簡單的設定</h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    簡易設定，讓你輕鬆上手
+                  </p>
+                </div>
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 bg-black bg-opacity-50 rounded-full">
+                    >
            <svg
             className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
             fill="currentColor"
@@ -43,14 +50,15 @@ return (
           >
             <path d="M7 3a1 1 0 000 2h6a1 1 0 100-2H7zM4 7a1 1 0 011-1h10a1 1 0 110 2H5a1 1 0 01-1-1zM2 11a2 2 0 012-2h12a2 2 0 012 2v4a2 2 0 01-2 2H4a2 2 0 01-2-2v-4z" />
           </svg>
-        </div>
-        <h3 className="mb-2 text-xl font-bold dark:text-white">多人同時看</h3>
-        <p className="text-gray-500 dark:text-gray-400">
-         提供多人同時觀看
-        </p>
-      </div>
-      <div>
-        <div className="flex justify-center items-center mb-4 w-10 h-10 rounded-full bg-primary-100 lg:h-12 lg:w-12 dark:bg-primary-900">
+                  </div>
+                  <h2 className="text-xl font-bold text-white">多人同時看</h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    提供多人使用同帳號同時觀看
+                  </p>
+                </div>
+                <div className="flex flex-col items-center space-y-2 border-gray-800 p-4 rounded-lg">
+                  <div className="p-2 bg-black bg-opacity-50 rounded-full">
+                    >
          <svg
             className="w-5 h-5 text-primary-600 lg:w-6 lg:h-6 dark:text-primary-300"
             fill="currentColor"
@@ -64,17 +72,23 @@ return (
               clipRule="evenodd"
             />
           </svg>
+                      <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
+                      <circle cx="12" cy="12" r="3" />
+                    </svg>
+                  </div>
+                  <h2 className="text-xl font-bold text-white">一個價格</h2>
+                  <p className="text-zinc-200 dark:text-zinc-100">
+                    便宜價格即可獲得所有功能，不分任何版本
+                  </p>
+                </div>
+                
+                </div>
+              </div>
+            </div>
+          </div>
         </div>
-        <h3 className="mb-2 text-xl font-bold dark:text-white">
-         價格親民
-        </h3>
-        <p className="text-gray-500 dark:text-gray-400">
-         便宜的價格
-        </p>
       </div>
-    </div>
-  </div>
-</section>
+    </section>
   )
 }
 
