1fd47b1e2c9ed32fd1cefcae20bc5dc72bdd3062
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-12 11:25:05 +0800

    Update watch-tv.tsx

--- pages/watch-tv.tsx
+++ pages/watch-tv.tsx
@@ -1,5 +1,6 @@
 import { useSession } from "next-auth/react"
 import { Button } from 'flowbite-react';
+import { signIn } from "next-auth/react"
 export default function Admin() {
   const { status } = useSession({
     required: true,
