f4adaef0b1dd9c7b4a2856ab238f6720743c3689
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-12 11:21:16 +0800

    Update watch-tv.tsx

--- pages/watch-tv.tsx
+++ pages/watch-tv.tsx
@@ -1,5 +1,5 @@
 import { useSession } from "next-auth/react"
-
+import { Button } from 'flowbite-react';
 export default function Admin() {
   const { status } = useSession({
     required: true,
@@ -9,7 +9,16 @@ export default function Admin() {
   })
 
   if (status === "loading") {
-    return "Loading or not authenticated..."
+    return
+    <section class="bg-white dark:bg-gray-900">
+    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div class="mx-auto max-w-screen-sm text-center">
+            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">請登入帳號</h1>
+                <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">要觀看tv請先登入謝謝。</p>
+            <button color="blue" onClick={() => signIn()}>Sign in</button>
+        </div>   
+    </div>
+</section>
   }
 
   return "請購買帳戶"
