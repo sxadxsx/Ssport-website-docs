b1ec5699690d119bbdfc2c91e743c797f4493160
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-12 11:23:38 +0800

    Update watch-tv.tsx

--- pages/watch-tv.tsx
+++ pages/watch-tv.tsx
@@ -10,11 +10,11 @@ export default function Admin() {
 
   if (status === "loading") {
     return
-    <section class="bg-white dark:bg-gray-900">
-    <div class="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
-        <div class="mx-auto max-w-screen-sm text-center">
-            <h1 class="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">請登入帳號</h1>
-                <p class="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">要觀看tv請先登入謝謝。</p>
+    <section className="bg-white dark:bg-gray-900">
+    <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
+        <div className="mx-auto max-w-screen-sm text-center">
+            <h1 className="mb-4 text-7xl tracking-tight font-extrabold lg:text-9xl text-primary-600 dark:text-primary-500">請登入帳號</h1>
+                <p className="mb-4 text-lg font-light text-gray-500 dark:text-gray-400">要觀看tv請先登入謝謝。</p>
             <button color="blue" onClick={() => signIn()}>Sign in</button>
         </div>   
     </div>
