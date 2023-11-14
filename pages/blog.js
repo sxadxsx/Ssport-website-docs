1d905fab573e32702448265542a5f0f3ccf9fb3f
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-14 21:47:36 +0800

    Update blog.js

--- pages/blog.js
+++ pages/blog.js
@@ -26,7 +26,6 @@ export default function Blog({posts}){
     </div>
   </div>
 </section>
-export const Foo = () => (
   <section className="bg-white dark:bg-gray-900">
     <div className="py-8 px-4 mx-auto max-w-screen-xl lg:py-16 lg:px-6">
       
