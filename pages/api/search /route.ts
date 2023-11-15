a62b3f9e6b90e2701ccabc00cc8004d2d1b4f734
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-15 22:29:14 +0800

    Update route.ts

--- pages/api/search /route.ts
+++ pages/api/search /route.ts
@@ -1,10 +1,11 @@
+import { getPageUrl } from '@/app/source'
 import { allDocs } from 'contentlayer/generated'
 import { createSearchAPI } from 'next-docs-zeta/search/server'
  
 export const { GET } = createSearchAPI('simple', {
-  indexes: allDocs.map(docs => ({
-    title: docs.title,
-    content: docs.body.raw,
-    url: '/docs/' + docs.slug
+  indexes: allDocs.map(page => ({
+    title: page.title,
+    content: page.body.raw,
+    url: getPageUrl(page.slug)
   }))
 })
