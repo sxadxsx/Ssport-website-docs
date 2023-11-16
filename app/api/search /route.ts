7a986daafd50326da307dca9468660cc6867fc9e
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-16 21:55:03 +0800

    Update route.ts

--- app/api/search /route.ts
+++ app/api/search /route.ts
@@ -1,7 +1,7 @@
 import { getPageUrl } from '@/app/source'
 import { allDocs } from 'contentlayer/generated'
 import { createSearchAPI } from 'next-docs-zeta/search/server'
- 
+
 export const { GET } = createSearchAPI('simple', {
   indexes: allDocs.map(page => ({
     title: page.title,
