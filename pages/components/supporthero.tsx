ba358fc9e82f31e2effc86107b8a405c8e2519fa
雙龍體育status 機器人 <107471917+sxadxsx@users.noreply.github.com>
2023-11-17 16:41:23 +0800

    Update supporthero.tsx

--- pages/components/supporthero.tsx
+++ pages/components/supporthero.tsx
@@ -1,4 +1,6 @@
 import styles from './Button.module.css';
+import { DocSearch } from '@docsearch/react'
+import a from './docsearch.module.css';
 
 
 function supporthero() {
@@ -10,6 +12,13 @@ function supporthero() {
     <h2 className="mb-4 text-4xl font-extrabold tracking-tight leading-none text-gray-900 md:text-5xl lg:text-6xl dark:text-white">
       需要 <span className={styles.abc}>雙龍體育</span>的幫助嗎?
     </h2>
+    <div className={a.docSearch}>
+            <DocSearch
+              apiKey="c2e792c2e75fe1dd3e40574f8b4c9a80"
+              appId="70GEOCJCSX"
+              indexName="help"
+            />
+          </div>    
   </div>
 </section>
 
