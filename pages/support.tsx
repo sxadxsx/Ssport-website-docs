4640eba9362647365974070a125d6a62153a5606
Peter yang <92738287+peteryang1756@users.noreply.github.com>
2023-11-17 17:57:11 +0800

    Update support.tsx

--- pages/support.tsx
+++ pages/support.tsx
@@ -3,7 +3,8 @@ import Supporthero from './components/supporthero';
 import Styles from './components/docsearch.module.css';
 import Ticket from './components/ticket';
 import Head from 'next/head'
-
+import { DocSearch } from '@docsearch/react'
+import '@docsearch/css';
 const support = () => {
   return (
        <>
